5db3330e9150d3e4785135e748557ccc34c674f2
Anh <anhpt379@gmail.com>
2020-03-12 10:40:30 +0100

    Fix low contrast fish commands

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -16,3 +16,31 @@ set -gx PATH $PATH $HOME/.krew/bin
 
 set -gx PATH $HOME/Library/Python/3.7/bin $PATH
 set -gx fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+
+# Base16 Eighties color scheme
+set -U fish_color_normal normal
+set -U fish_color_command 99cc99
+set -U fish_color_quote ffcc66
+set -U fish_color_redirection d3d0c8
+set -U fish_color_end cc99cc
+set -U fish_color_error f2777a
+set -U fish_color_param d3d0c8
+set -U fish_color_comment ffcc66
+set -U fish_color_match 6699cc
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator 6699cc
+set -U fish_color_escape 66cccc
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 747369
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+
