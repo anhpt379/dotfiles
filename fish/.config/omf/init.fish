98123dece548d59d94fe786cadad8c3ab0e0e947
Anh <anhpt379@gmail.com>
2020-05-04 18:03:31 +0200

    Disable multi-select in fzf by default

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -36,10 +36,14 @@ set -gx fish_user_paths '/usr/local/opt/curl/bin' $fish_user_paths
 
 # Fzf
 set -U FZF_COMPLETE 2
-set -U FZF_FIND_FILE_COMMAND 'fd --type f --hidden --exclude ".git" \$dir'
+set -U FZF_FIND_FILE_COMMAND 'fd --type f --hidden --exclude ".git" . \$dir'
 set -U FZF_CD_COMMAND 'fd --type d --hidden --exclude ".git"'
 set -U FZF_PREVIEW_FILE_CMD 'bat --color=always --style=numbers --line-range :300'
 set -U FZF_PREVIEW_DIR_CMD 'exa -la'
+set -U FZF_DEFAULT_COMMAND 'fd --type f --hidden --exclude ".git"'
+set -U FZF_DEFAULT_OPTS '--no-multi --reverse --height=40% --info=default'
+set -U FZF_FIND_FILE_OPTS $FZF_DEFAULT_OPTS
+
 
 # Custom fish color scheme
 set -U fish_color_normal normal
