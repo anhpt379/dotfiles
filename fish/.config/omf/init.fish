3f7b0d599c8dcfc468a6d3c8fbb61d34c3aa444a
Anh <anhpt379@gmail.com>
2020-05-13 08:06:45 +0200

    Fish custom vi-mode key bindings
    These keybindings are easier to use than the default
    fish_vi_key_bindings

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -48,8 +48,19 @@ balias c 'clear'
 balias j 'z'
 balias f 'fzf'
 
-bind \ce accept-autosuggestion
+# key bindings
+bind \cf accept-autosuggestion
 bind \cg accept-autosuggestion execute
+
+bind \ck kill-line            # (default)
+bind \cu backward-kill-line   # (default)
+bind \cw kill-word            # zsh-like ctrl+w
+bind \u222B backward-word     # option+b (vim 'b')
+bind \u2211 forward-word      # option+w (vim 'w')
+bind \u02D9 backward-char     # option+h (vim 'h')
+bind \u00AC forward-char      # option+l (vim 'l')
+bind \u00A2 end-of-line       # option+4 (vim '$')
+bind \u00A7 beginning-of-line # option+6 (vim '^')
 
 set -x GOROOT /usr/local/opt/go/libexec
 set -x GOPATH $HOME/.go
@@ -60,7 +71,7 @@ set -gx PATH $HOME/.cargo/bin $PATH
 set -gx fish_user_paths '/usr/local/opt/curl/bin' $fish_user_paths
 
 # Fzf
-bind \cf 'fzf_find'
+bind \co 'fzf_find'
 bind \cr 'fzf_history'
 
 set -gx FZF_DEFAULT_COMMAND        'fd --hidden --type f --exclude ".git"'
