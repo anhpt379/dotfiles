c656bd4984dbc5bec4077314913a86ee0250f6f4
Anh <anhpt379@gmail.com>
2020-05-30 23:34:16 +0200

    Fix can't show commit that fits in 1 screen in gl

--- fish/.config/fish/functions/gl.fish
+++ fish/.config/fish/functions/gl.fish
@@ -1,6 +1,6 @@
 function gl --description "Git browse commits"
     set -l log_line_to_hash "echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
-    set -l view_commit "$log_line_to_hash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy | less --tabs=4 -RFX'"
+    set -l view_commit "$log_line_to_hash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy | less --tabs=4 -RX'"
     set -l copy_commit_hash "$log_line_to_hash | tr -d '\n' | pbcopy"
 
     set open_in_browser "$log_line_to_hash | xargs -I % sh -c 'open https://\$(git config remote.origin.url | sed \'s/^git@//\' | sed \'s/\.git\$//\' | sed \'s/:/\//\')/commit/%'"
