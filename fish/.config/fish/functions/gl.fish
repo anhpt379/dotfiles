d775f43e61bfb6cff1ef5b2bb2676f397ed5f6aa
Anh <anhpt379@gmail.com>
2020-05-31 09:42:02 +0200

    Allow filtering full commit messages in fzf gl

--- fish/.config/fish/functions/gl.fish
+++ fish/.config/fish/functions/gl.fish
@@ -6,8 +6,13 @@ function gl --description "Git browse commits"
 
     set open_in_browser "$log_line_to_hash | xargs -I % sh -c 'open https://\$(git config remote.origin.url | sed \'s/^git@//\' | sed \'s/\.git\$//\' | sed \'s/:/\//\')/commit/%'"
 
-    git log -5000 --no-merges --color=always --format="%C(auto)%h%d %C(reset)%s %C(#555555)(%aN - %cr)" | \
-        fzf --no-sort --reverse --tiebreak=index --no-multi --ansi --height=100% \
+    git log -5000 \
+      --no-merges \
+      --color=always \
+      --format="%C(auto)%h %C(reset)%s %C(#555555)%b(%aN - %cr)" \
+      | tr '\n' ' ' \
+      | sed -E 's/[a-f0-9]{7,}+/\\n&/2g' \
+      | fzf --no-sort --reverse --tiebreak=index --no-multi --ansi --height=100% \
             --preview="$preview_commit" \
             --header="ENTER to view, CTRL-Y to copy hash, CTRL-O to open in browser, ESC to exit" \
             --bind "enter:execute:$view_commit" \
