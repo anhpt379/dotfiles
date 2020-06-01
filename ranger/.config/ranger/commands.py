d85e802c58f226bad77dcc102a433d11bfb7c8e7
Anh <anhpt379@gmail.com>
2020-06-01 21:15:30 +0200

    ripgrep ranger integration

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -36,8 +36,8 @@ class fzf_select(Command):
 
 class add_file(Command):
     def execute(self):
+        import os
         import subprocess
-        import os.path
 
         fname = self.arg(1)
 
@@ -52,4 +52,30 @@ class add_file(Command):
 
     def tab(self, tabnum):
         return self._tab_directory_content()
+
+
+class fzf_ripgrep(Command):
+    """
+    :fzf_ripgrep
+
+    Usage: fzf_ripgrep <search string>
+    """
+    def execute(self):
+        if self.arg(1):
+            search_string = self.rest(1)
+        else:
+            self.fm.notify("Usage: fzf_ripgrep <search string>", bad=True)
+            return
+
+        import subprocess
+        import os.path
+        from ranger.container.file import File
+
+        current_path = self.fm.thisfile.path
+        command="rg --smart-case --hidden --glob '!.git' '%s' %s | fzf | awk -F':' '{print $1}'" % (search_string, current_path)
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            self.fm.select_file(fzf_file)
 
