5893a81742e2bc18d56f31605e77445435341341
Anh <anhpt379@gmail.com>
2020-06-05 09:38:41 +0200

    Fzf git checkout branch

--- fish/.config/fish/functions/gl.fish
+++ fish/.config/fish/functions/gl.fish
@@ -1,22 +1,24 @@
 function gl --description "Git browse commits"
-    set -l log_line_to_hash "echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
-    set -l view_commit      "$log_line_to_hash | xargs -I % sh -c 'git show --color=always %'"
-    set -l preview_commit   "$log_line_to_hash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy | less --tabs=4 -RX'"
-    set -l copy_commit_hash "$log_line_to_hash | tr -d '\n' | pbcopy"
+  git rev-parse HEAD > /dev/null 2>&1 || return
 
-    set open_in_browser "$log_line_to_hash | xargs -I % sh -c 'open https://\$(git config remote.origin.url | sed \'s/^git@//\' | sed \'s/\.git\$//\' | sed \'s/:/\//\')/commit/%'"
+  set -l log_line_to_hash "echo {} | grep -o '[a-f0-9]\{7\}' | head -1"
+  set -l view_commit      "$log_line_to_hash | xargs -I % sh -c 'git show --color=always %'"
+  set -l preview_commit   "$log_line_to_hash | xargs -I % sh -c 'git show --color=always % | diff-so-fancy | less --tabs=4 -RX'"
+  set -l copy_commit_hash "$log_line_to_hash | tr -d '\n' | pbcopy"
 
-    git log -5000 \
-      --no-merges \
-      --color=always \
-      --format="%C(auto)%h %C(reset)%s %C(#555555)%b(%aN - %cr)" \
-      | tr '\n' ' ' \
-      | sed -E 's/[a-f0-9]{7,}+/\\n&/2g' \
-      | fzf --reverse --tiebreak=index --no-multi --ansi --height=100% \
-            --preview="$preview_commit" \
-            --header=" CTRL-S to toggle sort, CTRL-Y to copy hash, CTRL-O to open in browser" \
-            --bind="enter:execute:$view_commit" \
-            --bind="ctrl-y:execute-silent($copy_commit_hash)+abort" \
-            --bind="ctrl-o:execute-silent($open_in_browser)+abort" \
-            --bind="ctrl-s:toggle-sort"
+  set open_in_browser "$log_line_to_hash | xargs -I % sh -c 'open https://\$(git config remote.origin.url | sed \'s/^git@//\' | sed \'s/\.git\$//\' | sed \'s/:/\//\')/commit/%'"
+
+  git log -5000 \
+    --no-merges \
+    --color=always \
+    --format="%C(auto)%h %C(reset)%s %C(#555555)%b(%aN - %cr)" \
+    | tr '\n' ' ' \
+    | sed -E 's/[a-f0-9]{7,}+/\\n&/2g' \
+    | fzf --reverse --tiebreak=index --no-multi --ansi --height=100% \
+          --preview="$preview_commit" \
+          --header=" CTRL-S to toggle sort, CTRL-Y to copy hash, CTRL-O to open in browser" \
+          --bind="enter:execute:$view_commit" \
+          --bind="ctrl-y:execute-silent($copy_commit_hash)+abort" \
+          --bind="ctrl-o:execute-silent($open_in_browser)+abort" \
+          --bind="ctrl-s:toggle-sort"
 end
