297fe6bcd3b466f9a47b4e4193c08f9d9c0c98f6
Anh <anhpt379@gmail.com>
2020-06-07 10:39:27 +0200

    Colored man output

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -111,12 +111,12 @@ set -U fish_pager_color_progress brwhite --background=cyan
 # colored man output
 # from http://linuxtidbits.wordpress.com/2009/03/23/less-colors-for-man-pages/
 setenv LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-setenv LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
+setenv LESS_TERMCAP_md \e'[01;38;5;80m'  # begin bold
 setenv LESS_TERMCAP_me \e'[0m'           # end mode
 setenv LESS_TERMCAP_se \e'[0m'           # end standout-mode
-setenv LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
+setenv LESS_TERMCAP_so \e'[30;5;43m'     # begin standout-mode - info box
 setenv LESS_TERMCAP_ue \e'[0m'           # end underline
-setenv LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+setenv LESS_TERMCAP_us \e'[04;38;5;74m'  # begin underline
 
 # lf
 set -x LF_ICONS "\
