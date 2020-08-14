bcafd9645daa06d060a1fe46129fe2c8d5d14a94
Anh <anhpt379@gmail.com>
2020-08-14 06:35:24 +0200

    Using nvim for notes

---
+++ fish/.config/fish/functions/note.fish
@@ -0,0 +1,18 @@
+function note -d "Manage notes in ~/Workspace/notes"
+    set current_dir (pwd)
+
+    cd ~/Workspace/notes/
+    set file_to_open (
+        echo (
+            fd | devicon-lookup | \
+            fzf --preview="bat --color=always --line-range :100 (echo {} | cut -d' ' -f2-)" \
+                --preview-window=right:90% \
+                --height=100%
+        ) | cut -d' ' -f2-
+    )
+    if string length -q -- "$file_to_open"
+        nvim $file_to_open
+    end
+
+    cd $current_dir
+end
