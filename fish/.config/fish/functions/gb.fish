85a2d43388e6cdb93f9a022a1991a57821f2c7e0
Anh <anhpt379@gmail.com>
2020-08-15 16:56:19 +0200

    gb copy/delete branch

--- fish/.config/fish/functions/gb.fish
+++ fish/.config/fish/functions/gb.fish
@@ -1,12 +1,25 @@
 function gb --description 'Fuzzy-find and checkout a branch'
-    git rev-parse HEAD || return
+    git rev-parse HEAD >/dev/null || return
 
-    git branch --sort=-committerdate | \
-        grep -v HEAD | \
-        string trim | \
-        fzf --preview-window=right:75% --height=100% --preview \
-            'git log -n 50 --color=always --date=relative --abbrev=7 \
-                --pretty="format:%C(auto,blue)%>(12,trunc)%ad %C(auto,yellow)%h %C(auto,green)%aN %C(auto,reset)%s%C(auto,red)% gD% D" \
-                (echo {} | sed "s/.* //")' | \
-        read -l result; and git checkout "$result"
+    set branch_name "echo -n {} | sed 's/.* //'"
+    set preview_cmd "git log -n 50 \
+                        --color=always \
+                        --date=relative \
+                        --abbrev=7 \
+                        --pretty='format:%C(auto,blue)%>(12,trunc)%ad %C(auto,yellow)%h %C(auto,green)%aN %C(auto,reset)%s%C(auto,red)% gD% D' \
+                        (echo {} | sed 's/.* //')"
+    set copy_branch "$branch_name | pbcopy"
+    set checkout_branch "git checkout ($branch_name)"
+    set delete_branch "git branch -d ($branch_name)"
+
+    git branch --sort=-committerdate \
+      | grep -v HEAD \
+      | string trim \
+      | fzf --height=100% --preview-window=right:75% \
+        --print-query \
+        --preview="$preview_cmd" \
+        --header=" CTRL-Y to copy, CTRL-D to delete, ENTER to checkout" \
+        --bind="enter:execute/$checkout_branch/+abort" \
+        --bind="ctrl-d:execute/$delete_branch/+abort" \
+        --bind="ctrl-y:execute-silent/$copy_branch/+abort"
 end
