220cc169952f25992be0a3b8e16aacc878eb2ca7
Anh <anhpt379@gmail.com>
2020-09-26 10:58:14 +0200

    Preview the first line of each note in fzf window

--- fish/.config/fish/functions/note.fish
+++ fish/.config/fish/functions/note.fish
@@ -4,12 +4,12 @@ function note -d "Manage notes in ~/Workspace/notes"
     cd ~/Workspace/notes/
     set file_to_open (
         echo (
-            fd | devicon-lookup | \
-            fzf --preview="bat --color=always --line-range :100 (echo {} | cut -d' ' -f2-)" \
-                --preview-window=right:90% \
-                --height=100% --print-query | \
+            for i in *; preview=(head $i | awk '!/^ *#/ && NF' | head -1 | awk -v len=50 '{ if (length($0) > len) print substr($0, 1, len-3) "..."; else print; }') echo -e "$i \e[2m$preview\e[0m"; end | devicon-lookup | \
+            fzf --preview="bat --color=always --line-range :100 (echo {} | cut -d' ' -f2)" \
+                --preview-window=right:70% \
+                --height=100% --print-query --ansi | \
             tail -1
-        ) | cut -d' ' -f2-
+        ) | cut -d' ' -f2
     )
     if string length -q -- "$file_to_open"
         nvim $file_to_open
