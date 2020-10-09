a9f1821e85b81469f5a5976308314350f4c40463
Anh <anhpt379@gmail.com>
2020-10-09 11:01:39 +0200

    Make SSH password prompt clearer

--- fish/.config/fish/functions/ssh.fish
+++ fish/.config/fish/functions/ssh.fish
@@ -1,9 +1,12 @@
 function ssh -d "Make sure we have all the keys before ssh to a host"
     if not ssh-add -l | grep anhpt379@gmail.com | grep -q ED25519
+      echo "GitHub's SSH key expired. Adding it again..."
       ssh-add ~/.ssh/id_ed25519
+      echo
     end
 
     if not ssh-add -l | grep -q 'Booking.com temporary key'
+      echo "Booking's SSH key expired. Getting a new one..."
       command ssh -A ssh.booking.com
     end
 
