a697d3c1fe0da1c60295fb6cb14a26fb01382d84
Anh <anhpt379@gmail.com>
2020-11-17 12:21:05 +0100

    Vale coc-diagnostic integration
    Run vale to check for writing style issues in text and gitcommit
    buffers.

---
+++ nvim/.config/nvim/bin/vale.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import tempfile
+import subprocess
+
+text = sys.stdin.read()
+fd, path = tempfile.mkstemp()
+try:
+    with os.fdopen(fd, 'w') as tmp:
+        # do stuff with temp file
+        tmp.write(text)
+    command = f"vale --output=JSON --no-exit {path}"
+    status, output = subprocess.getstatusoutput(command)
+    if status != 0:
+        sys.exit(1)
+    data = json.loads(output)
+
+    errors = []
+    for match in data[path]:
+        error = {
+            'line': match['Line'],
+            'column': match['Span'][0],
+            'message': match['Message'] + '\n' + match['Link'],
+            'level': match['Severity'],
+        }
+        errors.append(error)
+
+    print(json.dumps(errors))
+finally:
+    os.remove(path)
