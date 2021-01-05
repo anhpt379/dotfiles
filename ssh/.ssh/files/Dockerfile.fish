2d75eb4f8c62836d23cd3bc55d0b5542cef97c87
Anh <anhpt379@gmail.com>
2021-01-05 14:52:45 +0100

    Upgrade fish to the nightly version
    To fix no `type` command in remote fish due to this commit [1] removed
    type.fish from /usr/local/share/fish/functions/
    
    [1]: https://github.com/fish-shell/fish-shell/commit/ef9c924

--- ssh/.ssh/files/Dockerfile.fish
+++ ssh/.ssh/files/Dockerfile.fish
@@ -1,6 +1,6 @@
 FROM centos:centos7.9.2009
 
-RUN curl 'https://download.opensuse.org/repositories/shells:/fish:/release:/3/RHEL_7/shells:fish:release:3.repo' -o /etc/yum.repos.d/fish.repo
+RUN curl 'https://download.opensuse.org/repositories/shells:/fish:/nightly:/master/CentOS_7/shells:fish:nightly:master.repo' -o /etc/yum.repos.d/fish.repo
 
 RUN yum update -y && yum install -y fish && yum clean all
 
