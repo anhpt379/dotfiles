70e813b645af4a4fb85b9d24b871ef5abadbc870
Anh <anhpt379@gmail.com>
2021-01-05 23:12:47 +0100

    Replace devicon-lookup rust with my custom one
    It works better with the new fzf_complete function (has dir icons and
    skips adding icon if lines start with `-`)

--- fish/.config/fish/functions/fzf_find.fish
+++ fish/.config/fish/functions/fzf_find.fish
@@ -1,43 +1,18 @@
 function fzf_find -d "Find files and folders"
-    set -l max_depth $argv[1]
-    set -l max_results 1000
-    set -l cmd (commandline -p)
-
     set -l commandline (__fzf_parse_commandline)
     set -l dir $commandline[1]
     set -l fzf_query $commandline[2]
 
-    set -l fd_command "fd --color=always --max-results=$max_results --no-ignore --hidden --exclude='.git' . $dir"
-
-    if test -n "$max_depth"
-        set -a fd_command "--max-depth=$max_depth"
-        if test "$dir" = "."
-            set tiebreak "begin"
-        else
-            set tiebreak "end"
-        end
-    else
-        set tiebreak "end"
-    end
-
-    if string match -q "cd*" $cmd
-        set -a fd_command "--type=d"
-    end
-
-    set fd_results (eval $fd_command)
-    if [ (count $fd_results) -eq $max_results ]
-        echo ""
-        echo (set_color 3BB0E0)Too many results. Showing the current dir only...(set_color normal)
-        set -a fd_command "--max-depth=1"
-        set fd_results (eval $fd_command)
-    end
-
-    if [ (count $fd_results) -eq 0 ]
-        commandline -it -- " "
-        return
-    end
-
-    set -l result (printf "%s\n" $fd_results | devicon-lookup | fzf --ansi --exact +m -1 --tiebreak=$tiebreak --expect=enter --header="(Press TAB to accept, ENTER to accept and run)" --bind=tab:accept --preview="~/.config/fzf/preview.sh {}" --query "$fzf_query")
+    set result (
+        fd --color=always --type=f --no-ignore --hidden --exclude='.git' . $dir \
+        | ~/.config/fzf/devicon-lookup.py \
+        | fzf --delimiter=\t --select-1 --exit-0 --exact --ansi \
+              --bind=tab:accept \
+              --expect=enter \
+              --header="(Press TAB to accept, ENTER to accept and run)" \
+              --preview="~/.config/fzf/preview.sh {}" \
+              --query "$fzf_query" \
+    )
 
     if test -z "$result"
         commandline -f repaint
@@ -48,16 +23,9 @@ function fzf_find -d "Find files and folders"
 
     set -l key (echo $result | cut -d' ' -f1)
     set -l result (echo $result | cut -d' ' -f3-)
+    commandline -it -- (string escape $result)
     if test "$key" = "enter"
-        commandline -it -- (string escape $result)
         commandline -f execute
-    else
-        commandline -it -- (string escape $result)
-        if test -d "$result"
-            commandline -it -- "/"
-        else
-            commandline -it -- ""
-        end
     end
     commandline -f repaint
 end
