e9780d48cfbc0d942eebb931b825096c6f6731a1
Anh <anhpt379@gmail.com>
2021-01-07 18:43:46 +0100

    Fix ssh doesn't work
    Because it couldn't locate `bash` shell
    
    ssh -v to a host shows:
    ```
    bash: No such file or directory
    debug1: Local version string SSH-2.0-OpenSSH_8.1
    ```

--- fish/.config/omf/init.fish
+++ fish/.config/omf/init.fish
@@ -117,7 +117,7 @@ bind \t  'fzf_complete'
 # Fix fzf couldn't preview matches that end with `\`. This happens in fish
 # only. Fzf uses the shell set in SHELL env to execute the preview command.
 # https://github.com/junegunn/fzf/blob/0.25.0/src/util/util_unix.go#L13
-set -gx SHELL 'bash'
+set -gx SHELL '/bin/bash'
 
 set -gx FZF_DEFAULT_COMMAND 'fd --hidden --type f --exclude ".git" -E "*.jp*g" -E "*.png"'
 set -gx FZF_DEFAULT_OPTS    '
