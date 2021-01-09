ef8b73e603c469b5a3808586b5f75813066c682f
Anh <anhpt379@gmail.com>
2021-01-09 18:52:16 +0100

    Prettify fzf_history script

--- fish/.config/fish/functions/fzf_history.fish
+++ fish/.config/fish/functions/fzf_history.fish
@@ -1,5 +1,13 @@
 function fzf_history -d "Show command history"
-    set -l result (history -z | fzf --tiebreak=index --expect=enter --header="(Press TAB to accept, ENTER to accept and run)" --bind=tab:accept --read0 -q (commandline))
+    set -l result (
+        history -z \
+        | fzf --tiebreak=index \
+              --expect=enter \
+              --header="(Press TAB to accept, ENTER to accept and run)" \
+              --bind=tab:accept \
+              --read0 \
+              --query=(commandline)
+    )
 
     set -l key (echo $result | cut -d' ' -f1)
     set -l result (echo $result | cut -d' ' -f2-)
