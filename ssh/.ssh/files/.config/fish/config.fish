42c4e2ec8cc6bd65228d67d01ca86767d9434f7e
Anh <anhpt379@gmail.com>
2021-02-05 16:53:08 +0100

    Fix some servers ask for sudo password on login
    In some servers, I have sudo, but on some allowed commands only. The
    previous way of checking doesn't work on those servers.

--- ssh/.ssh/files/.config/fish/config.fish
+++ ssh/.ssh/files/.config/fish/config.fish
@@ -4,24 +4,24 @@ set -gx PATH $HOME/.local/bin $PATH
 set -gx PATH $HOME/.local/bin/nvim/squashfs-root/usr/bin $PATH
 
 if not test -e ~/.local/bin/nvim/squashfs-root/usr/bin/nvim
-  cd ~/.local/bin/
-  mkdir -p nvim
-  cd nvim
-  ../nvim.appimage --appimage-extract
-  cd ~
+    cd ~/.local/bin/
+    mkdir -p nvim
+    cd nvim
+    ../nvim.appimage --appimage-extract
+    cd ~
 end
 
 source ~/.config/omf/init.fish
 
-alias pp  'command sudo puppet agent -t'
-alias ppa 'command sudo puppet agent -t --environment=(whoami)'
+alias pp 'command sudo puppet agent -t'
+alias ppa 'command sudo puppet agent -t --environment=$USER'
 
 alias jc='sudo journalctl -u'
 alias sc='sudo systemctl'
 
-alias tmux 'command tmux attach -t (whoami); or command tmux new -s (whoami)'
+alias tmux 'command tmux attach -t $USER; or command tmux new -s $USER'
 
-alias pbcopy  'nc 127.0.0.1 2224 --send-only'
+alias pbcopy 'nc 127.0.0.1 2224 --send-only'
 alias pbpaste 'nc 127.0.0.1 2225 --recv-only'
 
 alias root 'sudo -E -s'
@@ -29,8 +29,8 @@ alias sudo 'sudo -E'
 
 # Fix nvim `Cannot open undo file for writing` sometimes
 mkdir -p ~/.config/nvim/undo/
-if command sudo -v 2>/dev/null
-    sudo chown -R (whoami) ~/.config/nvim/undo/
+if [ (find . \! -user $USER -print | wc -l) -gt 0 ]
+    sudo chown -R $USER ~/.config/nvim/undo/
 end
 
 # Shorten title
