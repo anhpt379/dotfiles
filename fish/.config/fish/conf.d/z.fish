640898eafd5c38d1244ea462698989418ecd426c
Anh <anhpt379@gmail.com>
2021-02-05 18:29:08 +0100

    Update z to the latest version
    https://github.com/jethrokuan/z

--- fish/.config/fish/conf.d/z.fish
+++ fish/.config/fish/conf.d/z.fish
@@ -1,7 +1,7 @@
 if test -z "$Z_DATA"
   if test -z "$XDG_DATA_HOME"
     set -U Z_DATA_DIR "$HOME/.local/share/z"
-  else
+  else 
     set -U Z_DATA_DIR "$XDG_DATA_HOME/z"
   end
   set -U Z_DATA "$Z_DATA_DIR/data"
@@ -9,7 +9,7 @@ end
 
 if test ! -e "$Z_DATA"
   if test ! -e "$Z_DATA_DIR"
-    mkdir -p -m 700 "$Z_DATA_DIR"
+    mkdir -p -m 700 "$Z_DATA_DIR"  
   end
   touch "$Z_DATA"
 end
@@ -33,7 +33,10 @@ if test ! -z $ZO_CMD
 end
 
 if not set -q Z_EXCLUDE
-  set -U Z_EXCLUDE $HOME
+  set -U Z_EXCLUDE "^$HOME\$"
+else if contains $HOME $Z_EXCLUDE
+  # Workaround: migrate old default values to a regex (see #90).
+  set Z_EXCLUDE (string replace -r -- "^$HOME\$" '^'$HOME'$$' $Z_EXCLUDE)
 end
 
 # Setup completions once first
@@ -41,4 +44,20 @@ __z_complete
 
 function __z_on_variable_pwd --on-variable PWD
   __z_add
+end
+
+function __z_uninstall --on-event z_uninstall
+  functions -e __z_on_variable_pwd
+  functions -e $Z_CMD
+  functions -e $ZO_CMD
+
+  if test ! -z "$Z_DATA"
+      printf "To completely erase z's data, remove:\n" > /dev/stderr
+      printf "%s\n" "$Z_DATA" > /dev/stderr
+  end
+
+  set -e Z_CMD
+  set -e ZO_CMD
+  set -e Z_DATA
+  set -e Z_EXCLUDE
 end
