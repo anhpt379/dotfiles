27d163663d6aea6d07b19b46529ad5ecc1e624ca
Anh <anhpt379@gmail.com>
2021-05-07 18:04:47 +0200

    Fix broken fish on servers (happened sometimes)

--- fish/.config/fish/functions/rsync_dotfiles.sh
+++ fish/.config/fish/functions/rsync_dotfiles.sh
@@ -29,15 +29,14 @@ if [ ! -f "${lock_file}" ]; then
         --copy-links \
         --keep-dirlinks \
         --exclude-from="$HOME/.ssh/files/.rsyncignore" \
-        ~/.ssh/files/ "${target}":~/ 2>/dev/null
+        ~/.ssh/files/ "${target}":~/ 2>/dev/null && \
+        command ssh "${target}" -- 'touch ~/.rsync-done'
     echo "***************************************************************************"
 
     echo "***************************************************************************"
     echo "Removing some accidentally deployed files"
     command ssh "${target}" -- 'rm -f ~/.config/fish/functions/less.fish'
     echo "***************************************************************************"
-
-    command ssh "${target}" -- 'touch ~/.rsync-done'
 
     rm -f "${lock_file}"
 fi
