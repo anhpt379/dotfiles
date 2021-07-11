170965a15846daf81fe0b36cfa38fbeba2350f37
Anh <anhpt379@gmail.com>
2021-07-11 10:08:13 +0200

    More bandwidth to rsync dotfiles
    My wifi is faster now

--- fish/.config/fish/functions/rsync_dotfiles.sh
+++ fish/.config/fish/functions/rsync_dotfiles.sh
@@ -9,7 +9,7 @@ if [ ! -f "${lock_file}" ]; then
     echo "***************************************************************************"
     echo "Uploading ~/.local/bin/"
     rsync -azvhP \
-        --bwlimit=20 \
+        --bwlimit=100 \
         --info=name0 \
         --info=progress2 \
         --no-inc-recursive \
@@ -21,7 +21,7 @@ if [ ! -f "${lock_file}" ]; then
     echo "***************************************************************************"
     echo "Uploading ~/.ssh/files/"
     rsync -azvhP \
-        --bwlimit=20 \
+        --bwlimit=100 \
         --info=name0 \
         --info=progress2 \
         --no-inc-recursive \
