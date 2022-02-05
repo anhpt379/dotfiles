2118d4b161f82eaef3b6fd780d5ec0675e28bc95
Anh <anhpt379@gmail.com>
2022-02-05 20:32:39 +0100

    Simplify rsync_dotfiles script
    We have all binaries in ~/.ssh/files/.local/bin/ already, no need to
    copy them again from ~/.local/bin/, because there are many unrelated files in
    that directory now.

--- linux/fish/.config/fish/functions/rsync_dotfiles.sh
+++ linux/fish/.config/fish/functions/rsync_dotfiles.sh
@@ -7,18 +7,6 @@ if [ ! -f "${lock_file}" ] || [ "$(find "${lock_file}" -mtime +4h -print)" ]; th
     touch "${lock_file}"
 
     echo "***************************************************************************"
-    echo "Uploading ~/.local/bin/"
-    rsync -azvhP \
-        --bwlimit=50 \
-        --info=name0 \
-        --info=progress2 \
-        --no-inc-recursive \
-        --compress-level=9 \
-        --copy-links \
-        --keep-dirlinks \
-        ~/.local/bin/ "${target}":~/.local/bin/ 2>/dev/null
-
-    echo "***************************************************************************"
     echo "Uploading ~/.ssh/files/"
     rsync -azvhP \
         --bwlimit=50 \
