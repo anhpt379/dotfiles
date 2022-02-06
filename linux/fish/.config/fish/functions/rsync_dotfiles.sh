17475d3ba06108b010750fd704560632022ad055
Anh <anhpt379@gmail.com>
2022-02-06 10:19:04 +0100

    Make rsync_dotfiles run faster
    By ignoring existing files on the remote side

--- linux/fish/.config/fish/functions/rsync_dotfiles.sh
+++ linux/fish/.config/fish/functions/rsync_dotfiles.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
+set -x
 
 target=$1
 lock_file="/tmp/.rsync-${target}.lock"
 
-if [ ! -f "${lock_file}" ] || [ "$(find "${lock_file}" -mtime +4h -print)" ]; then
+if [ ! -f "${lock_file}" ] || [ "$(find "${lock_file}" -mmin +240 -print)" ]; then
     touch "${lock_file}"
 
     echo "***************************************************************************"
     echo "Uploading ~/.ssh/files/"
     rsync -azvhP \
         --bwlimit=50 \
-        --info=name0 \
-        --info=progress2 \
         --no-inc-recursive \
         --compress-level=9 \
         --copy-links \
         --keep-dirlinks \
+        --ignore-existing \
         --exclude-from="$HOME/.ssh/files/.rsyncignore" \
         ~/.ssh/files/ "${target}":~/ 2>/dev/null && \
         command ssh "${target}" -- 'touch ~/.rsync-done'
