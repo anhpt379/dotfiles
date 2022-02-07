ee721276a19a9f3461ba301d2be32b8a392aecd4
Anh <anhpt379@gmail.com>
2022-02-07 17:53:42 +0100

    feat(fish): a new fish prompt

--- linux/fish/.config/fish/functions/fish_prompt.fish
+++ linux/fish/.config/fish/functions/fish_prompt.fish
@@ -1,24 +1,9 @@
-# You can override some default options with config.fish:
-#
-#  set -g theme_short_path yes
-
 function fish_prompt
     set -l last_command_status $status
-    set -l cwd
-    set -l fish
 
-    if test "$theme_short_path" = yes
-        set cwd (basename (prompt_pwd))
-    else
-        set cwd (prompt_pwd)
-    end
+    set -l cwd (prompt_pwd)
+    set -l kernel (uname -s)
 
-    if [ (uname -s) = 'Darwin' ]
-        set fish ""
-        set is_darwin
-    else
-        set fish "⋊>"
-    end
     set -l ahead "↑"
     set -l behind "↓"
     set -l diverged "⥄ "
@@ -26,47 +11,42 @@ function fish_prompt
     set -l none "◦"
 
     set -l normal_color (set_color normal)
-    set -l success_color (set_color cyan)
-    set -l error_color (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
-    set -l directory_color (set_color white --bold)
     set -l repository_color (set_color yellow)
+    set -l directory_background_color (set_color -b 464646)
+    set -l directory_color
+    set -l user_color
+
+    if test $last_command_status -eq 0
+        set directory_color (set_color fefefe)
+    else
+        set directory_color (set_color eb1454)
+    end
+
+    if test $kernel = "Darwin"
+        echo -n -s " "
+    end
 
     if string match -q -- "*.*" (hostname)
         if test "$USER" = root
-            set color red
+            set user_color (set_color -b 333; set_color eb1454)
         else
-            set color white
+            set user_color (set_color -b 333; set_color fefefe)
         end
-        if test $last_command_status -eq 0
-            set fish_icon_color cyan
-        else
-            set fish_icon_color red
-        end
-        echo -n -s (set_color $color --bold) [$USER@(prompt_hostname) " " $cwd](set_color $fish_icon_color)$fish
+        echo -n -s $user_color (set_color --bold) " " $USER@(prompt_hostname) " " (set_color normal) $directory_background_color $directory_color " " $cwd " " $normal_color
     else
-        if test $last_command_status -eq 0
-            echo -n -s (set_color cyan) $fish (set_color -b normal)
-        else
-            echo -n -s (set_color red) $fish (set_color -b normal)
-        end
+        if test $kernel = "Linux"; and git_is_repo
+            echo -n -s $directory_background_color $directory_color " " $cwd " " $normal_color
+            echo -n -s " " $repository_color (git_branch_name)
 
-        if not set -q is_darwin; and git_is_repo
-            if test "$theme_short_path" = yes
-                set root_folder (command git rev-parse --show-toplevel 2> /dev/null)
-                set parent_root_folder (dirname $root_folder)
-                set cwd (echo $PWD | sed -e "s|$parent_root_folder/||")
-            end
-
-            echo -n -s " " $directory_color $cwd $normal_color
-            echo -n -s " on " $repository_color (git_branch_name)
+            set -l git_ahead_status (git_ahead $ahead $behind $diverged $none)
 
             if git_is_touched
                 echo -n -s " " $normal_color $dirty
-            else if git_ahead > /dev/null
-                echo -n -s " " $normal_color (git_ahead $ahead $behind $diverged $none)
+            else if test "$git_ahead_status" != ""
+                echo -n -s " " $normal_color $git_ahead_status
             end
         else
-            echo -n -s " " $directory_color $cwd
+            echo -n -s $directory_background_color $directory_color " " $cwd " "
         end
     end
 
