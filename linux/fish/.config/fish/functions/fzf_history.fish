8298499d1f98f759572405a57b7fc78f7f9a605a
Anh <anhpt379@gmail.com>
2022-02-09 09:09:23 +0100

    fix(fzf_history): incorrect repainting
    When using ENTER to select and execute a command, the text on screen
    didn't get repainted correctly. The repaint command should run right
    after modifying the text, not after executing.

--- linux/fish/.config/fish/functions/fzf_history.fish
+++ linux/fish/.config/fish/functions/fzf_history.fish
@@ -13,10 +13,13 @@ function fzf_history -d "Show command history"
     set -l result (echo $result | cut -d' ' -f2-)
     if test "$key" = enter
         commandline -- $result
+        commandline -f repaint
         commandline -f execute
     else if test -n $result
         commandline -- $result
         commandline -it -- " "
+        commandline -f repaint
+    else
+        commandline -f repaint
     end
-    commandline -f repaint
 end
