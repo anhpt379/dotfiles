c637fb1e9021f22b403e813dff7df7e66d2dd09c
Anh <anhpt379@gmail.com>
2022-04-02 21:48:27 +0200

    fix(view): `view <file>` didn't work

--- macOS/kitty/.config/kitty/pager.sh
+++ macOS/kitty/.config/kitty/pager.sh
@@ -14,12 +14,12 @@ fi
 exec /usr/local/bin/nvim \
   -u NONE \
   -c "map <silent> q :<C-u>qa!<CR>" \
-  -c "set shell=bash scrollback=100000 laststatus=0 clipboard+=unnamedplus" \
+  -c "set shell=bash scrollback=100000 laststatus=0 clipboard+=unnamedplus nowrap" \
   -c "autocmd TermEnter * stopinsert" \
   -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
   -c "nmap P ?^ \\\|^\[.*@.*\]<CR>" \
   -c "nmap <CR>  :nohlsearch<CR>" \
   -c "nmap <ESC> :nohlsearch<CR>" \
-  -c "nmap a <nop>" \
-  -c "nmap i <nop>" \
+  -c "nmap H zH | nmap L zL" \
+  -c "nmap a <nop> | nmap i <nop>" \
   -c 'terminal sed </dev/fd/63 -e "s/'$'\x1b'']8;;file:[^\]*[\]//g" && sleep 0.01 && printf "'$'\x1b'']2;"' 63<&0 0</dev/null
