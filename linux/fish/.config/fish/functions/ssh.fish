8c90110b87f3d1960cfa54ef2b607877ca061864
Anh <anhpt379@gmail.com>
2022-04-07 12:09:25 +0200

    fix(rsync): incorrect relative path on remote
    We need to put `./` to where we want to split the path:
    
    ```
    -R, --relative
    
    It is also possible to limit the amount of path information that is sent
    as implied directories for each path you specify. With a modern rsync on
    the sending side (beginning with 2.6.7), you can insert a dot and a
    slash into the source path, like this:
    
      rsync -avR /foo/./bar/baz.c remote:/tmp/
    ```

--- linux/fish/.config/fish/functions/ssh.fish
+++ linux/fish/.config/fish/functions/ssh.fish
@@ -35,7 +35,7 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
             --copy-links \
             --keep-dirlinks \
             --relative \
-            ~/.ssh/files/.{bashrc,inputrc,vimrc,less,terminfo,.local/bin/pbcopy,.local/bin/pbpaste} "$argv[1]":/tmp/panh/ 2>/dev/null
+            ~/.ssh/files/./.{bashrc,inputrc,vimrc,less,terminfo,local/bin/pbcopy,local/bin/pbpaste} "$argv[1]":/tmp/panh/ 2>/dev/null
 
         command ssh $argv -t HOME=/tmp/panh bash
 
@@ -50,7 +50,7 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
                 --copy-links \
                 --keep-dirlinks \
                 --relative \
-                ~/.ssh/files/.{bashrc,inputrc,vimrc,less,terminfo,.local/bin/pbcopy,.local/bin/pbpaste} "$argv[1]":~/ 2>/dev/null
+                ~/.ssh/files/./.{bashrc,inputrc,vimrc,less,terminfo,local/bin/pbcopy,local/bin/pbpaste} "$argv[1]":~/ 2>/dev/null
 
             mkdir -p ~/.cache/rsync
             nohup ~/.config/fish/functions/rsync_dotfiles.sh $argv > ~/.cache/rsync/$argv[1].log &
