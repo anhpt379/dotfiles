1e32c22f66e9da91a8f59df4fa288e4637aa68d7
Anh <anhpt379@gmail.com>
2022-04-11 16:35:18 +0200

    cut(puppet-lint): simplify puppet-lint checks
    The default ones are good enough for me (for now).

--- linux/bootstrap.sh
+++ linux/bootstrap.sh
@@ -64,76 +64,8 @@ sudo npm install -g neovim
 # null-ls
 pip install vim-vint 'ansible-lint[community,yamllint]'
 sudo npm install -g jsonlint textlint write-good markdownlint-cli
-gem install --bindir ~/.local/bin/ \
-  mdl rubocop \
-  puppet puppet-lint \
-  puppet-lint-absolute_classname-check \
-  puppet-lint-absolute_template_path \
-  puppet-lint-alias-check \
-  puppet-lint-anchor-check \
-  puppet-lint-appends-check \
-  puppet-lint-array_formatting-check \
-  puppet-lint-class_parameter-check \
-  puppet-lint-classes_and_types_beginning_with_digits-check \
-  puppet-lint-concatenated_template_files-check \
-  puppet-lint-cuddled_else-check \
-  puppet-lint-duplicate_class_parameters-check \
-  puppet-lint-ec2_facts-check \
-  puppet-lint-empty_lines_around_body-check \
-  puppet-lint-empty_string-check \
-  puppet-lint-empty_trailing_lines \
-  puppet-lint-exec_idempotent-check \
-  puppet-lint-explicit_hiera_class_param_lookup-check \
-  puppet-lint-extended \
-  puppet-lint-file_ensure-check \
-  puppet-lint-file_line_match-check \
-  puppet-lint-file_source_rights-check \
-  puppet-lint-fileserver-check \
-  puppet-lint-global_resource-check \
-  puppet-lint-i18n \
-  puppet-lint-leading_zero-check \
-  puppet-lint-legacy_fact-check \
-  puppet-lint-legacy_facts-check \
-  puppet-lint-lookup_in_parameter-check \
-  puppet-lint-manifest_whitespace-check \
-  puppet-lint-no_chaining_arrows-check \
-  puppet-lint-no_cron_resources-check \
-  puppet-lint-no_erb_template-check \
-  puppet-lint-no_file_path_attribute-check \
-  puppet-lint-no_symbolic_file_modes-check \
-  puppet-lint-non_erb_template_filename-check \
-  puppet-lint-numericvariable \
-  puppet-lint-optional_default-check \
-  puppet-lint-package_ensure-check \
-  puppet-lint-param-docs \
-  puppet-lint-param-types \
-  puppet-lint-parameter_type-check \
-  puppet-lint-params_empty_string-check \
-  puppet-lint-racism_terminology-check \
-  puppet-lint-recurse_file-check \
-  puppet-lint-resource_outside_class-check \
-  puppet-lint-resource_reference_syntax \
-  puppet-lint-roles-profiles \
-  puppet-lint-roles_and_profiles-check \
-  puppet-lint-security-plugins \
-  puppet-lint-spaceship_operator_without_tag-check \
-  puppet-lint-strict_indent-check \
-  puppet-lint-template_file_extension-check \
-  puppet-lint-top_scope_facts-check \
-  puppet-lint-topscope-variable-check \
-  puppet-lint-trailing_comma-check \
-  puppet-lint-trailing_newline-check \
-  puppet-lint-uncuddled_else-check \
-  puppet-lint-undef_in_function-check \
-  puppet-lint-unquoted_string-check \
-  puppet-lint-usascii_format-check \
-  puppet-lint-use_ensure_packages-check \
-  puppet-lint-variable_contains_upcase \
-  puppet-lint-version_comparison-check \
-  puppet-lint-wmf_styleguide-check \
-  puppet-lint-world_writable_files-check \
-  puppet-lint-yumrepo_gpgcheck_enabled-check
 sudo gem install --bindir ~/.local/bin/ rubocop rubocop-rspec
+gem install --bindir ~/.local/bin/ mdl puppet puppet-lint
 rm -f ~/.local/bin/puppet
 
 sudo luarocks install luacheck
