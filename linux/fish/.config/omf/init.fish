a693b0612eeb71673032bf6e3dbfbfa302bf8b3d
Anh <anhpt379@gmail.com>
2022-04-29 09:35:50 +0200

    feat(fish): new `sdb` command

--- linux/fish/.config/omf/init.fish
+++ linux/fish/.config/omf/init.fish
@@ -207,3 +207,15 @@ set -gx LF_ICONS (
     -e 's/$//'           \
   | tr '\n' ':'
 )
+
+# search a server by fqdn prefix and open it in serverdb
+function sdb
+    set -l query $argv[1]
+    set -l fqdn (cat ~/.cache/servers.txt | grep ^$query | head -1 | xargs)
+    if test -n $fqdn
+        set -l id (curl -s https://simpleapi.prod.$COMPANY_DOMAIN/servers/$fqdn | jq .server.id)
+        open https://serverdb.$COMPANY_DOMAIN/hud/v/servers/server/$id/
+    else
+        echo "No servers found"
+    end
+end
