b3576cf75aa3f4a93644bc7f7b1be11f2ac634fa
Anh <anhpt379@gmail.com>
2022-06-30 18:13:59 +0200

    refactor(lf): merge lf-preview.sh & fzf-preview.sh
    Let's use `~/.local/bin/fzf-preview.sh` for everything.

--- linux/lf/.local/bin/lf-preview.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if file -bL --mime "$@" | grep -q 'charset=binary'; then
-  dirname=$(dirname "$1")
-  filename=$(basename "$1")
-  cd "$dirname" && exa -lha --color=always "$filename"
-else
-  head -300 "$1" | nvimpager -c
-fi
