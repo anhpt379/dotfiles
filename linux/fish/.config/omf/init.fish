6353fc0c69abba1e31fb4990dba17fecb6482050
Anh <anhpt379@gmail.com>
2022-07-07 12:00:33 +0200

    fix(ansible): `tput: unknown terminal "xterm-kitty"`

--- linux/fish/.config/omf/init.fish
+++ linux/fish/.config/omf/init.fish
@@ -60,6 +60,9 @@ balias pwl 'nvim ~/.config/nvim/dictionaries/personal_word_list.txt'
 balias docker         'sudo -E docker'
 balias docker-compose 'sudo -E docker-compose'
 
+balias ansible          'TERM=xterm-256color command ansible'
+balias ansible-playbook 'TERM=xterm-256color command ansible-playbook'
+
 balias k   'kubectl'
 balias kd  'kubectl describe'
 balias kdf 'kubectl delete --force --grace-period=0'
