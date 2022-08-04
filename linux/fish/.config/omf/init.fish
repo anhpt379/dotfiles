3eafa6847a95c98511987dcd63d7fb8d72276d81
Anh <anhpt379@gmail.com>
2022-08-04 12:19:07 +0200

    fix(less): better 1-line big file detection
    Also, use `--quit-if-one-screen` by default.

--- linux/fish/.config/omf/init.fish
+++ linux/fish/.config/omf/init.fish
@@ -96,7 +96,7 @@ end
 
 function less
     if [ ! -t 1 ]
-        command less $argv
+        command less --quit-if-one-screen $argv
         return
     end
 
@@ -107,7 +107,7 @@ function less
             set -f file_size (command ls -s --block-size=1048576 "$file" | cut -d' ' -f1) # in MB
 
             # Open big files, or files with one really long line with less
-            if [ $file_size -gt 200 ] || [ "$(head -2 "$file" 2>/dev/null | wc -l)" -lt 2 ]
+            if [ $file_size -gt 200 ] || [ "$(head --bytes=4096 "$file" 2>/dev/null | head -1 | wc --chars)" -gt 1000 ]
                 command less --line-numbers $argv
                 return
             end
