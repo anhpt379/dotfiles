b92cb7450a24196b6f5ca3dfc0fe4d72bb04e887
Anh Pham <anhpt379@gmail.com>
2022-09-12 22:36:07 +0200

    fix(bootstrap): debian

--- local/bootstrap-linux.sh
+++ local/bootstrap-linux.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 sudo apt-get update --allow-releaseinfo-change
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 
 sudo apt-get install -y --no-install-recommends stow git \
   fzf jq ripgrep exa \
   curl ncat dnsutils \
   telnet atop corkscrew \
   grc gron pwgen tldr \
-  youtube-dl ipython \
-  python3-virtualenv python3-pip python3-dev \
-  cargo npm luarocks golang build-essential
+  youtube-dl ipython python3-virtualenv \
+  python3-setuptools python3-pip python3-dev \
+  cargo luarocks golang rubygems nodejs build-essential
 
 # dotfiles
 cd ~/ || exit 1
@@ -52,8 +53,6 @@ mv fd-*/fd ~/.local/bin/
 rm -rf fd-*
 
 # nvim
-# sudo npm i -g npm@latest
-
 sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
 git clone --depth=1 https://github.com/neovim/neovim.git
 cd neovim || exit 1
@@ -65,49 +64,44 @@ nvim -c "PlugInstall" -c "qall"
 nvim -c "TSUpdate" -c "qall"
 
 # neoformat
-go install mvdan.cc/sh/v3/cmd/shfmt@latest
+curl -fLo ~/.local/bin/shfmt https://github.com/mvdan/sh/releases/download/v3.5.1/shfmt_v3.5.1_linux_arm64 && chmod +x ~/.local/bin/shfmt
 pip3 install black
 
 # prettier
 sudo npm install -g --save-dev --save-exact prettier
 
 # python/ruby/node.js provider
-pip install neovim
+pip3 install neovim
 gem install --bindir ~/.local/bin/ neovim
 sudo npm install -g neovim
 
 # null-ls
-pip install vim-vint 'ansible-lint[community,yamllint]'
+pip3 install vim-vint
 sudo npm install -g jsonlint textlint write-good markdownlint-cli
-sudo gem install --bindir ~/.local/bin/ rubocop rubocop-rspec
-gem install --bindir ~/.local/bin/ mdl puppet puppet-lint puppet-lint-strict_indent-check
-rm -f ~/.local/bin/puppet
+gem install --bindir ~/.local/bin/ rubocop rubocop-rspec mdl
 
 sudo luarocks install luacheck
-sudo dnf install -y ShellCheck codespell proselint
-curl -fLo ~/.local/bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.8.0/hadolint-Linux-x86_64 && \
+sudo apt-get install -y shellcheck codespell python3-proselint
+curl -fLo ~/.local/bin/hadolint https://github.com/hadolint/hadolint/releases/download/v2.10.0/hadolint-Linux-arm64 && \
   chmod +x ~/.local/bin/hadolint
 
-# nvimpager
-sudo apt install -y scdoc
-git clone --depth=1 https://github.com/lucc/nvimpager.git
-cd nvimpager || exit 1
-make PREFIX=~/.local install
-cd ..
-rm -rf nvimpager
-
 # lf
-wget https://github.com/gokcehan/lf/releases/download/r26/lf-linux-amd64.tar.gz
-tar zxvf lf-linux-amd64.tar.gz
+cd /tmp || exit
+wget https://github.com/gokcehan/lf/releases/download/r27/lf-linux-arm64.tar.gz
+tar zxvf lf-*.tar.gz
 mv lf ~/.local/bin/
-rm -f lf-linux-amd64.tar.gz
+rm -f lf-*.tar.gz
+cd -
 
 # prettyping
 curl -fLo ~/.local/bin/prettyping https://github.com/denilsonsa/prettyping/raw/master/prettyping
 chmod +x ~/.local/bin/prettyping
 
 # xh
-cargo install xh
+wget https://github.com/ducaale/xh/releases/download/v0.16.1/xh-v0.16.1-aarch64-unknown-linux-musl.tar.gz
+tar zxvf xh-*.tar.gz
+mv xh-*/xh ~/.local/bin/
+rm -rf xh-*.tar.gz
 
 # grc-rs
 cargo install grc-rs
@@ -115,48 +109,29 @@ cargo install grc-rs
 # mocword
 cargo install mocword
 
-# fx
-sudo npm install -g fx
-
-# glow
-sudo rpm -i https://github.com/charmbracelet/glow/releases/download/v1.4.1/glow_1.4.1_linux_amd64.rpm
-
-# diff-so-fancy
-git clone --depth=1 https://github.com/so-fancy/diff-so-fancy.git
-mv diff-so-fancy/diff-so-fancy ~/.local/bin/
-mv diff-so-fancy/lib/ ~/.local/bin/lib
-rm -rf diff-so-fancy
-
 # docker
-sudo dnf install -y docker docker-compose
+sudo apt install -y docker docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
 sudo docker-compose -f ~/dotfiles/linux/docker-compose.yml up -d
 
-# puppet-editor-config
-cd ~/.local/share/nvim/lsp_servers/puppet || exit 1
-sed -i 's/\r//g' puppet-*
-
 # cron
-sudo dnf install -y cronie
-sudo systemctl enable crond
-sudo systemctl start crond
+# sudo dnf install -y cronie
+# sudo systemctl enable crond
+# sudo systemctl start crond
 
-echo "0 * * * * bash ~/Music/youtube-dl/download.sh" > cron
-# Use this cronjob to refresh the list of servers that fzf_find uses to search
-# for hosts when typing `ssh<C-f>`:
-# echo "0 3 * * * bash -c \"curl -fsH 'Accept-encoding: gzip' https://<DOMAIN>/servers | gunzip - | jq -r '.servers | .[] | .fqdn' | sort | grep -v 'spare-' > ~/.cache/servers.txt\" >>/tmp/cron.log 2>&1" >> cron
-crontab cron
-rm -f cron
+# echo "0 * * * * bash ~/Music/youtube-dl/download.sh" > cron
+# # Use this cronjob to refresh the list of servers that fzf_find uses to search
+# # for hosts when typing `ssh<C-f>`:
+# # echo "0 3 * * * bash -c \"curl -fsH 'Accept-encoding: gzip' https://<DOMAIN>/servers | gunzip - | jq -r '.servers | .[] | .fqdn' | sort | grep -v 'spare-' > ~/.cache/servers.txt\" >>/tmp/cron.log 2>&1" >> cron
+# crontab cron
+# rm -f cron
 
 # ffmpeg
-sudo dnf install -y \
-  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
-  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
-sudo dnf install -y ffmpeg
-
-# ssh
-mkdir ~/.ssh/control/
+# sudo dnf install -y \
+#   https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+#   https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+# sudo dnf install -y ffmpeg
 
 # timezone
 sudo timedatectl set-timezone Europe/Amsterdam
