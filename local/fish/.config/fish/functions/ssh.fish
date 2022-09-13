0b51dba3358d0a95bc76d799163ea410ebbfec94
Anh Pham <anhpt379@gmail.com>
2022-09-13 13:04:01 +0200

    fix(macOS): no more password for ssh private key

--- local/fish/.config/fish/functions/ssh.fish
+++ local/fish/.config/fish/functions/ssh.fish
@@ -9,7 +9,11 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
 
     if not ssh-add -l | grep anhpt379@gmail.com | grep -q ED25519
         echo "GitHub's SSH key expired. Adding it again..."
-        ssh-add ~/.ssh/id_ed25519
+        if begin string match -q -- "Darwin" (uname);
+            ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+        else
+            ssh-add ~/.ssh/id_ed25519
+        end
         echo
     end
 
