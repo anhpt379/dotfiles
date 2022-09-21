2ece94fef9c9bfea0a6f554af2de09698a695f5d
Anh Pham <anhpt379@gmail.com>
2022-09-21 15:42:37 +0200

    fix(fish): inefficient tab completion
    With the default completion, `cat /et<Tab>ng<Tab>` brings me to
    `/etc/nginx/` with 2 tabs, while with the custom one I need
    3 tabs (`cat /et<Tab>ng<Tab>in<Tab>`), and it was confusing, since
    sometimes I thought I could fuzzy match all files there, but the list
    didn't contain all files and failed the expectation.
    
    This updates the behavior to use the default completion most of the
    time, and open fzf sometimes, when it has all files listed. Now, when I
    see fzf, I know I can filter ALL files there.

--- local/fish/.config/fish/functions/fzf_complete.fish
+++ local/fish/.config/fish/functions/fzf_complete.fish
@@ -1,6 +1,6 @@
 function fzf_complete
     set -l command (commandline --cut-at-cursor)
-    set -l keyword (commandline -ct)
+    # set -l keyword (commandline -ct)
 
     # Trigger fzf_find instead if there're directories in the suggestion
     if complete -C "$command" | grep "/\$" >/dev/null
@@ -10,48 +10,6 @@ function fzf_complete
         end
     end
 
-    # Color descriptions manually
-    set -l result (
-        begin
-            complete -C "$command." | grep -Fv '/./' | grep -Fv '/../' | grep -Fv './' | grep -Fv '../'
-            complete -C "$command"
-        end \
-        | sort | uniq \
-        | string replace -r \t'(.*)$' \t$(tput setaf 8)'$1'$(tput sgr0) \
-        | fzf --delimiter=\t --select-1 --exit-0 --ansi \
-              --expect=enter \
-              --expect=tab \
-              --height=20% \
-              --tiebreak=chunk \
-              --header="$(tput setaf 1)TAB$(tput sgr0) to select, $(tput setaf 1)ENTER$(tput sgr0) to run, $(tput setaf 1)CTRL-[$(tput sgr0) to stop, $(tput setaf 1)CTRL-/$(tput sgr0) to toggle preview" \
-              --preview="$FZF_PREVIEW_COMMAND" \
-              --query "$keyword" \
-    )
-    if test $status -ne 0
-        commandline -f repaint
-        return
-    end
-
-    # Split key & result
-    set -l key (echo $result | cut -d' ' -f1)
-    set -l result (echo $result | cut -d' ' -f2-)
-
-    # Remove description
-    set -l result (string replace -r \t'.*' '' -- $result)
-
-    if test "$result" = ""
-        commandline -it -- ""
-        commandline -f repaint
-    else
-        if string match -q -- "*/" $result
-            commandline -rt -- (string replace --all " " "\\ " -- $result)
-        else
-            commandline -rt -- (string replace --all " " "\\ " -- $result)" "
-        end
-        commandline -f repaint
-
-        if test "$key" = enter
-            commandline -f execute
-        end
-    end
+    # Fallback to the default suggestion
+    commandline -f complete
 end
