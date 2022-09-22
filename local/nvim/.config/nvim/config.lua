3969cbb0f53ff5e6cb2345e6cec059a5fe1cac81
Anh Pham <anhpt379@gmail.com>
2022-09-22 07:53:33 +0200

    fix(nvim): unable to use `<C-n>`/`<C-p>` to browse vim command history
    It worked on the first key press, but on the second it selected a
    suggestion from by nvim-cmp instead.

--- local/nvim/.config/nvim/config.lua
+++ local/nvim/.config/nvim/config.lua
@@ -149,16 +149,6 @@ cmp.setup({
   })
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
 local lsp_status = require('lsp-status')
 lsp_status.register_progress()
 
