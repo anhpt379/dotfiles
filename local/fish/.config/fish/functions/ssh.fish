92e5db81fa78048e21a64fb57cb48071bdc60aae
Anh Pham <anhpt379@gmail.com>
2022-09-23 13:38:09 +0200

    chore(ssh): wording

--- local/fish/.config/fish/functions/ssh.fish
+++ local/fish/.config/fish/functions/ssh.fish
@@ -8,7 +8,7 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
     end
 
     if not ssh-add -l | grep anhpt379@gmail.com | grep -q ED25519
-        echo "GitHub's SSH key expired. Adding it again..."
+        echo "GitHub's SSH key not found. Adding it..."
         if string match -q -- Darwin (uname)
             ssh-add --apple-use-keychain ~/.ssh/id_ed25519
         else
