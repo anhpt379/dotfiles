849eb6349d3ddb133f3371bfb1b5646ae4c4eb70
Anh Pham <anhpt379@gmail.com>
2023-04-17 16:17:11 +0200

    fix(ssh): unable to clone dotfiles
    The error was "Permission denied"

--- local/fish/.config/fish/functions/ssh.fish
+++ local/fish/.config/fish/functions/ssh.fish
@@ -93,13 +93,12 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
 
         set GITLAB_DOMAIN (echo $WORK_EMAIL | awk -F@ '{ print "gitlab."$2 }')
         command ssh $argv -t "
-            GIT_SSH_COMMAND='ssh -i /usr/local/etc/gitlab_ssh_key_dotfiles/id_rsa'
             if test -d .files; then
                 cd .files/
-                git fetch --depth 1 origin master
+                GIT_SSH_COMMAND='ssh -i /usr/local/etc/gitlab_ssh_key_dotfiles/id_rsa' git fetch --depth 1 origin master
                 git reset --hard origin/master
             else
-                git clone --depth=1 --branch=master git@$GITLAB_DOMAIN:panh/dotfiles.git .files
+                GIT_SSH_COMMAND='ssh -i /usr/local/etc/gitlab_ssh_key_dotfiles/id_rsa' git clone --depth=1 --branch=master git@$GITLAB_DOMAIN:panh/dotfiles.git .files
                 cd .files/
             fi
 
