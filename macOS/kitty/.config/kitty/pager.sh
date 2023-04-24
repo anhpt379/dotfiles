17a1f5db4b701039003889441a67b573d54235e6
Anh Pham <anhpt379@gmail.com>
2023-04-24 11:52:28 +0200

    ui(view,pager): use blue as the selection color
    And enable true color & text highlighting on copying

--- macOS/kitty/.config/kitty/pager.sh
+++ macOS/kitty/.config/kitty/pager.sh
@@ -13,13 +13,11 @@ fi
 
 exec /opt/homebrew/bin/nvim \
   -u NONE \
-  -c "map <silent> q :<C-u>qa!<CR>" \
-  -c "set shell=bash scrollback=100000 laststatus=0 clipboard+=unnamedplus nowrap incsearch ignorecase smartcase mousescroll=ver:1,hor:0" \
+  -c "set shell=bash termguicolors scrollback=100000 laststatus=0 clipboard+=unnamedplus nowrap incsearch ignorecase smartcase mousescroll=ver:1,hor:0" \
+  -c "highlight Visual cterm=NONE ctermfg=0 ctermbg=12 gui=NONE guifg=#000000 guibg=#28c9ff | highlight NonText ctermfg=59 gui=NONE guifg=#555555 | highlight Search cterm=NONE ctermfg=0 ctermbg=214 gui=NONE guifg=#000000 guibg=#ffcc66 | highlight IncSearch cterm=NONE ctermfg=0 ctermbg=214 gui=NONE guifg=#000000 guibg=#fab955 | autocmd TextYankPost * silent! lua vim.highlight.on_yank {higroup=IncSearch, timeout=700}" \
   -c "autocmd TermEnter * stopinsert" \
   -c "autocmd TermClose * ${AUTOCMD_TERMCLOSE_CMD}" \
   -c "autocmd VimEnter * normal G" \
   -c "nmap p ? \\\|^\[.*\]<CR>" \
-  -c "nmap <CR>  :nohlsearch<CR> | nmap <ESC> :nohlsearch<CR>" \
-  -c "nmap H zH | nmap L zL | nmap J <C-e> | nmap K <C-y>" \
-  -c "nmap a <nop> | nmap i <nop>" \
+  -c "map <silent> q :<C-u>qa!<CR> | nmap <CR> :nohlsearch<CR> | nmap <ESC> :nohlsearch<CR> | nmap H zH | nmap L zL | nmap J <C-e> | nmap K <C-y> | nmap a <nop> | nmap i <nop>" \
   -c "silent write! /tmp/kitty_scrollback_buffer | terminal cat /tmp/kitty_scrollback_buffer -"
