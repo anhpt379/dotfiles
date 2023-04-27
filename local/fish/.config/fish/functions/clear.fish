f5604d5589ea3e3b5c12643de7e015f4885227e5
Anh Pham <anhpt379@gmail.com>
2023-04-27 22:18:18 +0200

    refactor(clear): move all logic to 1 fish function

--- local/fish/.config/fish/functions/clear.fish
+++ local/fish/.config/fish/functions/clear.fish
@@ -2,6 +2,17 @@ function clear
     if set -q TMUX
         tput reset
     else
-        command clear
+        # https://superuser.com/a/1667623
+        if test (count $argv) -ne 0
+            command clear "$argv"
+        else
+            set count (tput lines 2>/dev/null)
+            while test $count -gt 1
+                printf '\n'
+                set count (math $count - 1)
+            end
+
+            command clear -x
+        end
     end
 end
