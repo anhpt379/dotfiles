2d1f2f40d87fc8bf5d4bf7c11abbe99fadb29312
Anh Pham <anhpt379@gmail.com>
2023-04-28 15:15:47 +0200

    chore(remote): remove unused Dockerfile

--- remote/Dockerfile.fish
+++
@@ -1,7 +0,0 @@
-FROM centos:centos7.9.2009
-
-RUN curl 'https://download.opensuse.org/repositories/shells:/fish:/nightly:/master/CentOS_7/shells:fish:nightly:master.repo' -o /etc/yum.repos.d/fish.repo
-
-RUN yum install -y fish && yum clean all
-
-RUN fish --version
