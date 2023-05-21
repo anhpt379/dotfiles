ab2cf1d7a9d422b0a90ce94dbd9a0a808d326a33
Anh Pham <anhpt379@gmail.com>
2023-05-21 09:32:08 +0200

    fix(youtube-dl): remove random ids at the end of song names
    The filename will be used as the song name in iTunes.

--- macOS/youtube-dl/Music/youtube-dl/download.sh
+++ macOS/youtube-dl/Music/youtube-dl/download.sh
@@ -24,6 +24,7 @@ youtube-dl \
   --extract-audio \
   --audio-format m4a \
   --audio-quality 0 \
+  --output "%(title)s.%(ext)s" \
   --download-archive downloaded.txt \
   --batch-file urls.txt \
 
