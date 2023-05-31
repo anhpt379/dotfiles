a2af20a216154820d98e6fa023fdb8d40c2f1db3
Anh Pham <anhpt379@gmail.com>
2023-05-31 17:23:08 +0200

    bump(fzf): upgrade fzf to the latest version

--- local/bootstrap.sh
+++ local/bootstrap.sh
@@ -75,8 +75,8 @@ rm -rf fd-*
 
 # fzf
 cd /tmp || exit 1
-curl -fLo ~/.local/share/man/man1/fzf.1 https://raw.githubusercontent.com/junegunn/fzf/0.38.0/man/man1/fzf.1
-wget https://github.com/junegunn/fzf/releases/download/0.38.0/fzf-0.38.0-linux_arm64.tar.gz
+curl -fLo ~/.local/share/man/man1/fzf.1 https://raw.githubusercontent.com/junegunn/fzf/0.41.1/man/man1/fzf.1
+wget https://github.com/junegunn/fzf/releases/download/0.41.1/fzf-0.41.1-linux_arm64.tar.gz
 tar zxvf fzf-*.tar.gz
 mv fzf ~/.local/bin/
 rm -rf fzf-*.tar.gz
--- remote/README.md
+++ remote/README.md
@@ -17,7 +17,7 @@ chmod +x nvim.appimage
 mv nvim.appimage HOME/.local/bin/
 
 # fzf
-set FZF_VERSION 0.38.0
+set FZF_VERSION 0.41.1
 wget https://github.com/junegunn/fzf/releases/download/$FZF_VERSION/fzf-$FZF_VERSION-linux_amd64.tar.gz
 tar zxvf fzf-$FZF_VERSION-linux_amd64.tar.gz
 mv fzf HOME/.local/bin/
