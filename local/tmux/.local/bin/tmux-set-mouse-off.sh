aa5ee4096203d8548b530f53a96361c2066466bd
Anh Pham <anhpt379@gmail.com>
2023-07-07 21:29:15 +0200

    feat(tmux): higher set mouse off refresh rate
    `status-interval` doesn't accept less than 1 second value, so I have to
    work around it.

--- local/tmux/.local/bin/tmux-set-mouse-off.sh
+++ local/tmux/.local/bin/tmux-set-mouse-off.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-if tmux show mouse | grep -q on; then
-  tmux set -w mouse off
-fi
+for pid in $(pidof -x tmux-set-mouse-off.sh); do
+  if test "$pid" -ne $$; then
+    echo "Process is already running with PID $pid"
+    exit
+  fi
+done
+
+while true; do
+  if test "$(tmux show mouse)" = "mouse on"; then
+    tmux set -w mouse off
+  fi
+  sleep 0.5
+done
