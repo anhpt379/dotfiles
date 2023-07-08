ab1af2f6db89b3978d385894e013a344b29f0309
Anh Pham <anhpt379@gmail.com>
2023-07-08 13:56:24 +0200

    chore(fish): strip `~/` from tab title

--- local/fish/.config/fish/functions/fish_title.fish
+++ local/fish/.config/fish/functions/fish_title.fish
@@ -1,11 +1,10 @@
 function fish_title
     if string match -e -q -- fedora $(hostname); and test -n "$TMUX"
-        if test $_ = fish
-            set title $(prompt_pwd)
-        else if string match -r -- ".*ssh .*" $argv
+        if string match -r -- ".*ssh .*" $argv
             set title $(echo $argv  | cut -d. -f1-2)
         else
-            set title "$_ $(prompt_pwd)"
+            set cwd $(prompt_pwd | string replace -- "~/" "" | string replace -- "~" "")
+            set title "$_ $cwd"
         end
         tmux rename-window $(echo $title | python3 -c "import sys; print('{:^10}'.format(sys.stdin.read().strip()))")
     end
