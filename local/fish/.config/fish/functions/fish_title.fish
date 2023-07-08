8f4264f14a1aeb7248b609547259ea676f22b456
Anh Pham <anhpt379@gmail.com>
2023-07-08 11:48:57 +0200

    ui(fish): set a min width for all fish titles

--- local/fish/.config/fish/functions/fish_title.fish
+++ local/fish/.config/fish/functions/fish_title.fish
@@ -1,12 +1,12 @@
 function fish_title
     if string match -e -q -- fedora $(hostname); and test -n "$TMUX"
         if test $_ = fish
-            set cwd $(prompt_pwd | python3 -c "import sys; print('{:^10}'.format(sys.stdin.read().strip()))")
-            tmux rename-window $cwd
+            set title $(prompt_pwd)
         else if string match -r -- ".*ssh .*" $argv
-            tmux rename-window $(echo $argv  | cut -d. -f1-2)
+            set title $(echo $argv  | cut -d. -f1-2)
         else
-            tmux rename-window "$_ $(prompt_pwd)"
+            set title "$_ $(prompt_pwd)"
         end
+        tmux rename-window $(echo $title | python3 -c "import sys; print('{:^10}'.format(sys.stdin.read().strip()))")
     end
 end
