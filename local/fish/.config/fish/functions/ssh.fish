e186c85d1a5d048136089beba3458baf1c338bd3
Anh Pham <anhpt379@gmail.com>
2023-07-11 22:28:25 +0200

    refactor(ssh): use my credentials to clone dotfiles.git on remote servers

--- local/fish/.config/fish/functions/ssh.fish
+++ local/fish/.config/fish/functions/ssh.fish
@@ -41,10 +41,10 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
     set REMOTE_COMMAND "
         if test -d .files; then
             cd .files/
-            GIT_SSH_COMMAND='ssh -i /usr/local/etc/gitlab_ssh_key_dotfiles/id_rsa' git fetch --depth 1 origin master
+            git fetch --depth 1 origin master
             git reset --hard origin/master
         else
-            GIT_SSH_COMMAND='ssh -i /usr/local/etc/gitlab_ssh_key_dotfiles/id_rsa' git clone --depth=1 --branch=master git@$GITLAB_DOMAIN:panh/dotfiles.git .files
+            git clone --depth=1 --branch=master git@$GITLAB_DOMAIN:panh/dotfiles.git .files
             cd .files/
         fi
 
