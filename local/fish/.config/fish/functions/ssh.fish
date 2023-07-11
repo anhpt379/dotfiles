087ef4faad7a79af923daef0215e565e2cfffb8b
Anh Pham <anhpt379@gmail.com>
2023-07-11 22:38:11 +0200

    fix(ssh): failing to refresh ssh keys sometimes
    pgrep can verify if the process is running, but it can't verify if it's
    actually working or not. `ssh-add -l` does a better job on this.

--- local/fish/.config/fish/functions/ssh.fish
+++ local/fish/.config/fish/functions/ssh.fish
@@ -1,7 +1,8 @@
 function ssh -d "Make sure we have all the keys before ssh to a host"
     # Run ssh-agent via fish shell
     # https://gist.github.com/josh-padnick/c90183be3d0e1feb89afd7573505cab3
-    if test -z (pgrep ssh-agent | string collect)
+    if ! ssh-add -l >/dev/null
+        pkill ssh-agent
         eval (ssh-agent -c)
         set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK &>/dev/null
         set -Ux SSH_AGENT_PID $SSH_AGENT_PID &>/dev/null
