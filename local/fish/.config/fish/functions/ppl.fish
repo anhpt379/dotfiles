c88b80bf4ecef63c5175bed1722183279a417013
Anh Pham <anhpt379@gmail.com>
2023-09-20 22:29:24 +0200

    fix(ppl): unable to scroll with trackpad
    The `less` version on remote is too old and doesn't support mouse mode.

--- local/fish/.config/fish/functions/ppl.fish
+++ local/fish/.config/fish/functions/ppl.fish
@@ -2,8 +2,8 @@
 
 function ppl
     if test -t 1
-        /usr/bin/less /var/log/puppet/puppetagent.log
-    else
         nvimpager -p +\$ /var/log/puppet/puppetagent.log
+    else
+        /usr/bin/less /var/log/puppet/puppetagent.log
     end
 end
