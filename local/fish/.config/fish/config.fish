fc25b6a2d96703da6c1f49aef3aeffff80a9c444
Anh Pham <anhpt379@gmail.com>
2023-10-17 21:58:20 +0200

    fix(fish): failing to find `facter` command

--- local/fish/.config/fish/config.fish
+++ local/fish/.config/fish/config.fish
@@ -29,6 +29,7 @@ end
 
 fish_add_path /usr/sbin
 fish_add_path /usr/local/sbin
+fish_add_path /opt/puppetlabs/bin
 fish_add_path $HOME/.go/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
