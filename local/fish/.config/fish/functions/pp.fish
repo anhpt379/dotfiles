be60e392729b0fe0940a5e354ca77c9296a52cb3
Anh Pham <anhpt379@gmail.com>
2023-10-24 13:05:48 +0200

    fix(fish): incorrect `puppet` binary
    `/bin/puppet` is the main wrapper and we should prefer it over
    `/opt/puppetlabs/bin/puppet`. I can't add `/bin` to top of the path,
    because other binaries in `/bin` shouldn't be preferred.

--- local/fish/.config/fish/functions/pp.fish
+++ local/fish/.config/fish/functions/pp.fish
@@ -1,3 +1,3 @@
 function pp --wraps='command sudo HOME=/root TERMINFO=/home/panh/.terminfo puppet agent -t' --description 'alias pp command sudo HOME=/root TERMINFO=/home/panh/.terminfo puppet agent -t'
-    command sudo HOME=/root TERMINFO=/home/panh/.terminfo puppet agent -t $argv
+    command sudo HOME=/root TERMINFO=/home/panh/.terminfo /bin/puppet agent -t $argv
 end
