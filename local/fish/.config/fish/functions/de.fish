1d6f9a6b02e0db26045b9a2e1a4913eb9a539c5c
Anh Pham <anhpt379@gmail.com>
2023-11-02 14:30:41 +0100

    feat(fish): `pbcopy` now works in docker container
    It still requires `python3` to be available in the container though.

--- local/fish/.config/fish/functions/de.fish
+++ local/fish/.config/fish/functions/de.fish
@@ -3,6 +3,10 @@ function de -d "Copy dotfiles & exec to a docker container"
         docker cp ~/$i $argv[1]:/root/ >/dev/null
     end
 
+    for i in pbcopy
+        docker cp ~/.local/bin/$i $argv[1]:/usr/local/bin/ >/dev/null
+    end
+
     if test $(count $argv) -eq 1
         sudo -E docker exec -it $argv bash
     else
