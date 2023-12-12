5c53456e9d1a7438374b39210a893cc0abf6aa0a
Anh Pham <anhpt379@gmail.com>
2023-12-12 11:56:27 +0100

    chore(ssh): ask for passphrase only when needed
    No need to enter it every day.

--- local/fish/.config/fish/functions/ssh.fish
+++ local/fish/.config/fish/functions/ssh.fish
@@ -34,16 +34,6 @@ function ssh -d "Make sure we have all the keys before ssh to a host"
         end
     end
 
-    if not ssh-add -l | grep anhpt379@gmail.com | grep -q ED25519
-        echo "GitHub's SSH key not found. Adding it..."
-        if string match -q -- Darwin (uname)
-            ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-        else
-            ssh-add ~/.ssh/id_ed25519
-        end
-        echo
-    end
-
     if string match -q -- "root@*" $argv
         set REMOTE_HOME_DIR /tmp/panh
     else
