f16af7e8efa0ea1eab11d0e4028333aae9bc8077
Anh Pham <anhpt379@gmail.com>
2023-12-12 11:57:20 +0100

    fix(fish): `ppaf` doesn't work sometimes

--- local/fish/.config/fish/functions/ppaf.fish
+++ local/fish/.config/fish/functions/ppaf.fish
@@ -1,5 +1,6 @@
 function ppaf --wraps='sudo killall puppet 2>/dev/null; pp --enable; ppa' --description 'alias ppaf sudo killall puppet 2>/dev/null; pp --enable; ppa'
     sudo killall puppet 2>/dev/null
+    sleep 1
     pp --enable
     ppa $argv
 end
