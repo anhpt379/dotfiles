1178668d63a2211ff93259f7c432531de8fbf1d9
Anh Pham <anhpt379@gmail.com>
2023-12-16 10:07:10 +0100

    feat(fish): `reset` to retry fetching till successful

--- local/fish/.config/fish/functions/reset.fish
+++ local/fish/.config/fish/functions/reset.fish
@@ -1,6 +1,10 @@
 function reset
-    if ! git cat-file -e "$argv[1]" 2>/dev/null
-        git fetch origin
+    set sleep 0
+    while ! git cat-file -e "$argv[1]" 2>/dev/null
+        sleep $sleep
+        echo "$argv[1] not found locally. Fetching from remote..."
+        git fetch --all
+        set sleep 1
     end
     git reset --hard "$argv[1]"
 end
