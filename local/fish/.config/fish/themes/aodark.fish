d56948959fc9d5658f478ceb812ef8a37d0d496e
Anh Pham <anhpt379@gmail.com>
2023-12-30 11:13:36 +0100

    fix(kitty): incorrect aodark colors

--- local/fish/.config/fish/themes/aodark.fish
+++ local/fish/.config/fish/themes/aodark.fish
@@ -1,26 +1,26 @@
 # Custom fish color scheme
 set -U fish_color_normal normal
-set -U fish_color_command dcdfe4
-set -U fish_color_quote ffcc66
-set -U fish_color_redirection d3d0c8
-set -U fish_color_end cc99cc
-set -U fish_color_error dd3c69
-set -U fish_color_param dcdfe4
-set -U fish_color_comment ffcc66
-set -U fish_color_match 6699cc
+set -U fish_color_command white
+set -U fish_color_quote yellow
+set -U fish_color_redirection white
+set -U fish_color_end 696969
+set -U fish_color_error red
+set -U fish_color_param white
+set -U fish_color_comment yellow
+set -U fish_color_match 696969
 set -U fish_color_selection white --bold --background=brblack
 set -U fish_color_search_match bryellow --background=brblack
 set -U fish_color_history_current --bold
-set -U fish_color_operator 6699cc
-set -U fish_color_escape 66cccc
+set -U fish_color_operator 696969
+set -U fish_color_escape cyan
 set -U fish_color_cwd green
 set -U fish_color_cwd_root red
 set -U fish_color_valid_path --bold
-set -U fish_color_autosuggestion 747369
+set -U fish_color_autosuggestion 696969
 set -U fish_color_user brgreen
 set -U fish_color_host normal
 set -U fish_color_cancel -r
 set -U fish_pager_color_completion normal
-set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_description 696969 yellow
 set -U fish_pager_color_prefix white --bold --underline
 set -U fish_pager_color_progress brwhite --background=cyan
