ebd368432c4945462a37de72384c96fddd954a98
Anh Pham <anhpt379@gmail.com>
2024-04-20 22:20:23 +0200

    ui(macOS): change resolution to "More Space"
    The text I see it's still crisp, and Chrome address bar & Slack sidebar
    become smaller in this mode, which provides more screen space to display
    other content.

--- local/fish/.config/fish/functions/scroll_up.fish
+++ local/fish/.config/fish/functions/scroll_up.fish
@@ -1,13 +1,13 @@
 function scroll_up
     set -l screen_height $(tput lines)
-    if test "$screen_height" -ge 44
-        # Scroll up 40 lines from bottom when the screen is high enough.
+    if test "$screen_height" -ge 40
+        # Scroll up 35 lines from bottom when the screen is high enough.
         #
         # This helps my neck, since it hurts when looking at the bottom of the
         # screen most of the time.
         #
-        # We print 40 newlines, then moves the cursor back up 40 lines with the
+        # We print 35 newlines, then moves the cursor back up search 35 lines with the
         # \e[8A escape code, before printing the actual prompt.
-        printf '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\e[40A'
+        printf '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\e[35A'
     end
 end
