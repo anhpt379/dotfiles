a328e7d0b33f129049e0323a0557c3aa7a84536c
Anh Pham <anhpt379@gmail.com>
2024-04-22 21:24:34 +0200

    feat(firefox): add user.js

--- macOS/firefox/README.md
+++
@@ -1,4 +0,0 @@
-# Customizations
-
-1. <https://support.mozilla.org/en-US/questions/1394194>
-2. <https://support.mozilla.org/en-US/kb/compact-mode-workaround-firefox>
---
+++ macOS/firefox/user.js
@@ -0,0 +1,18 @@
+// Reduce tab bar's height
+user_pref("browser.compactmode.show", true);
+
+// Remove "List all Tabs" button on the right side of the tab bar
+user_pref("browser.tabs.tabmanager.enabled", false);
+
+// Open context menu search in background tab
+user_pref("browser.search.context.loadInBackground", true);
+
+// Disable Pocket
+user_pref("extensions.pocket.enabled", false);
+
+// Disable Google suggestions
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.urlbar.suggest.searches", false);
+
+// Highlight all words when searching
+user_pref("findbar.highlightAll", true);
