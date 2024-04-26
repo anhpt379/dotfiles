c4ce23e2c4fa4fbd6fcbcba4803c1b7d9473b01c
Anh Pham <anhpt379@gmail.com>
2024-04-26 09:39:15 +0200

    fix(tmux): incorrect tab title when opening a split

--- local/fish/.config/fish/functions/fish_title.fish
+++ local/fish/.config/fish/functions/fish_title.fish
@@ -1,17 +1,20 @@
 function fish_title
     if test -n "$TMUX"
-        if string match -r -- ".*ssh .*" $argv
-            set title $(echo $argv  | cut -d. -f1-2)
-        else
-            set cwd $(prompt_pwd | string replace -- "~/" "" | string replace -- "~" "" | xargs)
-            if test -n $cwd
-                set title "$_ $cwd"
+        # Only set tab title if this is the main pane
+        if test $(tmux display -p '#{pane_index}') -eq 1
+            if string match -r -- ".*ssh .*" $argv
+                set title $(echo $argv  | cut -d. -f1-2)
             else
-                set title "$_"
+                set cwd $(prompt_pwd | string replace -- "~/" "" | string replace -- "~" "" | xargs)
+                if test -n $cwd
+                    set title "$_ $cwd"
+                else
+                    set title "$_"
+                end
             end
-        end
 
-        printf "\ek$title\e\\"
+            printf "\ek$title\e\\"
+        end
     else
         if test $(uname -s) = Darwin
             echo -n " "
