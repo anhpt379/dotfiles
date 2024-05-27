14beac0b0e1eed2401a73637770948218965987c
Anh Pham <anhpt379@gmail.com>
2024-05-27 23:17:27 +0200

    ui(macOS): bigger font size for everything
    Hope that it will reduce my eye strain.

--- local/fish/.config/fish/functions/scroll_up.fish
+++ local/fish/.config/fish/functions/scroll_up.fish
@@ -1,13 +1,13 @@
 function scroll_up
     set -l screen_height $(tput lines)
-    if test "$screen_height" -ge 40
-        # Scroll up 35 lines from bottom when the screen is high enough.
+    if test "$screen_height" -ge 35
+        # Scroll up 30 lines from bottom when the screen is high enough.
         #
         # This helps my neck, since it hurts when looking at the bottom of the
         # screen most of the time.
         #
-        # We print 35 newlines, then moves the cursor back up search 35 lines with the
+        # We print 30 newlines, then moves the cursor back up search 30 lines with the
         # \e[8A escape code, before printing the actual prompt.
-        printf '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\e[35A'
+        printf '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\e[30A'
     end
 end
