291adc8ffce3c7ad1f81dd85ec20fa54f2f91441
Anh Pham <anhpt379@gmail.com>
2024-06-23 20:19:28 +0200

    ui(wezterm): remove ligatures
    On WezTerm we don't have `disable_ligatures cursor` option, which makes
    ligatures hard to understand. Let's disable it till we find a better
    way.

---
+++ macOS/fonts/comicfira.py
@@ -0,0 +1,46 @@
+import fontforge
+import psMat
+
+
+def copy_characters(source_font_path, dest_font_path, out_font_path, characters):
+    # Open source and destination fonts
+    source_font = fontforge.open(source_font_path)
+    dest_font = fontforge.open(dest_font_path)
+
+    dest_font.familyname = "ComicFira"
+    dest_font.fontname = "ComicFira"
+    dest_font.fullname = "ComicFira"
+
+    for character in characters:
+        # Check if the character exists in the source font
+        unicode_value = ord(character)
+        if unicode_value in source_font:
+            # Select glyph in source font
+            source_font.selection.select(unicode_value)
+
+            # Copy glyph from source to destination font
+            source_font.copy()
+            dest_font.selection.select(unicode_value)
+            dest_font.paste()
+        else:
+            print(f"Warning: '{character}' not found in source font.")
+
+    # Shift all charactoers down 10%
+    em_size = dest_font.em
+    shift_amount = -0.1 * em_size  # Negative to move down
+    for glyph in dest_font.glyphs():
+        glyph.transform(psMat.translate(0, shift_amount))
+
+    # Save the modified font
+    dest_font.generate(out_font_path)
+
+    # Close fonts
+    source_font.close()
+    dest_font.close()
+
+
+if __name__ == "__main__":
+    characters = ["<", "=", ">"]
+    copy_characters(
+        "FiraCode-Retina.ttf", "ComicMonoNF-Regular.ttf", "ComicFira.ttf", characters
+    )
