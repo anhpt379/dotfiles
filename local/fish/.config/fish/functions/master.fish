806d6eb1adc0d11c14e438a3f0678c64c03e097e
Anh Pham <anhpt379@gmail.com>
2024-08-15 16:52:35 +0200

    fix(fish): failing to detect `main` branch

--- local/fish/.config/fish/functions/master.fish
+++ local/fish/.config/fish/functions/master.fish
@@ -1,5 +1,5 @@
 function master
-    set default_branch_name (git branch | grep -o -m1 "\b\(master\|main\)\b")
+    set default_branch_name $(git branch | grep -Eo " \b(master|main)\b" | xargs)
     git checkout $default_branch_name
     git diff-index --quiet HEAD && git pull --rebase origin $default_branch_name
 end
