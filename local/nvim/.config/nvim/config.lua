3ca4ce9b621099bedf87eb98dcebb8ce926b10f6
Anh Pham <anhpt379@gmail.com>
2024-11-29 11:39:41 +0100

    feat(nvim): auto complete hacker news words

--- local/nvim/.config/nvim/config.lua
+++ local/nvim/.config/nvim/config.lua
@@ -409,64 +409,86 @@ cmp.setup {
   fuzzy = {
     use_typo_resistance = false,
   },
-
   sources = {
-    -- list of enabled providers
     completion = {
-      enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- Static list of providers to enable, or a function to dynamically enable/disable providers based on the context
+      enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep' },
+      -- Example dynamically picking providers based on the filetype and treesitter node:
+      -- enabled_providers = function(ctx)
+      --   local node = vim.treesitter.get_node()
+      --   if vim.bo.filetype == 'lua' then
+      --     return { 'lsp', 'path' }
+      --   elseif node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }), node:type())
+      --     return { 'buffer' }
+      --   else
+      --     return { 'lsp', 'path', 'snippets', 'buffer' }
+      --   end
+      -- end
     },
 
-    -- table of providers to configure
+    -- Please see https://github.com/Saghen/blink.compat for using `nvim-cmp` sources
     providers = {
       lsp = {
         name = 'LSP',
         module = 'blink.cmp.sources.lsp',
+        --- *All* of the providers have the following options available
+        --- NOTE: All of these options may be functions to get dynamic behavior
+        --- See the type definitions for more information.
+        --- Check the enabled_providers config for an example
+        enabled = true, -- Whether or not to enable the provider
+        transform_items = nil, -- Function to transform the items before they're returned
+        should_show_items = true, -- Whether or not to show the items
+        max_items = nil, -- Maximum number of items to display in the menu
+        min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
+        fallback_for = {}, -- If any of these providers return 0 items, it will fallback to this provider
+        score_offset = 0, -- Boost/penalize the score of the items
+        override = nil, -- Override the source's functions
       },
       path = {
         name = 'Path',
         module = 'blink.cmp.sources.path',
         score_offset = 3,
+        opts = {
+          trailing_slash = false,
+          label_trailing_slash = true,
+          get_cwd = function(context) return vim.fn.expand(('#%d:p:h'):format(context.bufnr)) end,
+          show_hidden_files_by_default = false,
+        }
       },
       snippets = {
         name = 'Snippets',
         module = 'blink.cmp.sources.snippets',
         score_offset = -3,
+        opts = {
+          friendly_snippets = true,
+          search_paths = { vim.fn.stdpath('config') .. '/snippets' },
+          global_snippets = { 'all' },
+          extended_filetypes = {},
+          ignored_filetypes = {},
+          get_filetype = function(context)
+            return vim.bo.filetype
+          end
+        }
+
+        --- Example usage for disabling the snippet provider after pressing trigger characters (i.e. ".")
+        -- enabled = function(ctx)
+        --   return ctx ~= nil and ctx.trigger.kind == vim.lsp.protocol.CompletionTriggerKind.TriggerCharacter
+        -- end,
       },
       buffer = {
         name = 'Buffer',
         module = 'blink.cmp.sources.buffer',
         fallback_for = { 'lsp' },
       },
-      -- ripgrep = {
-      --   name = 'Ripgrep',
-      --   -- module = 'blink-cmp-rg',
-      --   module = 'blink-ripgrep',
-      --   opts = {
-      --     prefix_min_len = 3,
-      --     context_size = 3,
-      --     -- get_command = function(context, prefix)
-      --     --   return {
-      --     --     'rg',
-      --     --     '--no-config',
-      --     --     '--json',
-      --     --     '--word-regexp',
-      --     --     '--ignore-case',
-      --     --     '--',
-      --     --     prefix .. '[\\w_-]+',
-      --     --     vim.fs.root(0, '.git') or vim.fn.getcwd(),
-      --     --   }
-      --     -- end,
-      --     -- get_prefix = function(context)
-      --     --   local col = vim.api.nvim_win_get_cursor(0)[2]
-      --     --   local line = vim.api.nvim_get_current_line()
-      --     --   local prefix = line:sub(1, col):match '[%w_-]+$' or ''
-      --     --   return prefix
-      --     -- end,
-      --   },
-      -- },
+      ripgrep = {
+        module = "blink-ripgrep",
+        name = "ripgrep",
+        opts = {
+          prefix_min_len = 2,
+        }
+      }
     },
   },
-
   windows = {
     autocomplete = {
       auto_show = true,
