9c0a3b135a0edd0dae128cb0b19c2ad7d6667a63
Anh Pham <anhpt379@gmail.com>
2024-12-31 11:21:59 +0100

    chore(reset): a more reliable way to pull non-existing commit

--- local/fish/.config/fish/functions/reset.fish
+++ local/fish/.config/fish/functions/reset.fish
@@ -3,7 +3,7 @@ function reset
     while ! git cat-file -e "$argv[1]" 2>/dev/null
         sleep $sleep
         echo "$argv[1] not found locally. Fetching from remote..."
-        git fetch origin 'refs/heads/panh*:refs/remotes/origin/panh*'
+        git fetch origin "$argv[1]"
         set sleep 1
     end
     git reset --hard "$argv[1]"
