1d48138f4b25a971a57164d84af2376dbe113fbe
Anh Pham <anhpt379@gmail.com>
2025-10-28 11:37:02 +0100

    fix(fish): option+delete shouldn't delete the entire word
    Now option+backspace/delete works on part of the word, while
    ctrl+backspace/delete works on the whole word

--- local/fish/.config/fish/functions/fish_user_key_bindings.fish
+++ local/fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,35 +1,35 @@
 function fish_user_key_bindings --description "User key bindings for fish"
     # Key bindings
-    bind \cf accept-autosuggestion execute
-    bind \cz undo
+    bind ctrl-f accept-autosuggestion execute
+    bind ctrl-z undo
 
     # Make Control+W (or Option+Delete) in fish works similar to vim. By default,
     # fish binds it to `backward-kill-path-component`, which doesn't respect the
     # `-` character in the middle of a word.
-    bind \cw backward-kill-bigword
+    bind ctrl-w backward-kill-bigword
+    bind ctrl-delete kill-bigword
 
-    # Make reverse word delete work (using ctrl/option+delete)
-    bind \e\[3\;5~ kill-bigword
-    bind \e\[3\;3~ kill-word
+    bind alt-backspace backward-kill-word
+    bind alt-delete kill-word
 
     # Use pageup/pagedown to jump between big words
-    bind \e\[5~ forward-bigword
-    bind \e\[6~ backward-bigword
+    bind ctrl-k forward-bigword
+    bind ctrl-j backward-bigword
 
     # Fix random command appears when pressing Option-N/P. This maps them to
     # Up/Down, to behave the same as Ctrl-N/P.
-    bind \en down-or-search
-    bind \ep up-or-search
+    bind ctrl-n down-or-search
+    bind ctrl-p up-or-search
 
     # Ctrl-D to exit immediately (the default behavior is `delete-or-exit`, which
     # is annoying since I can't exit the shell in the middle of typing a command)
-    bind \cd exit
+    bind ctrl-d exit
 
     # Edit and run the current command in vim
-    bind \cv edit_and_run_command_buffer
+    bind ctrl-v edit_and_run_command_buffer
 
     # Fzf
-    bind --erase \co
-    bind \t fzf_complete
-    bind \cr fzf_history
+    bind --erase ctrl-o
+    bind tab fzf_complete
+    bind ctrl-r fzf_history
 end
