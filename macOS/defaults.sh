b511f1fd8a2c48db236aa8d76c88e2130099259f
Anh Pham <anhpt379@gmail.com>
2025-11-27 23:55:33 +0100

    chore(macOS): showing dock faster
    This removes the delay when moving the cursor to the bottom of the
    screen.

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -42,6 +42,7 @@ defaults write com.apple.dock magnification -bool true
 defaults write com.apple.dock largesize -int 128
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-time-modifier -float 0.25
+defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock minimize-to-application -bool true
 defaults write com.apple.dock mru-spaces -bool false
 defaults write com.apple.dock show-recents -bool false
