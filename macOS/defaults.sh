5b825c7956db9ba264b53722aa409040ece089ec
Anh Pham <anhpt379@gmail.com>
2025-12-05 16:11:46 +0100

    feat(macOS): stop saving screenshots to desktop

--- macOS/defaults.sh
+++ macOS/defaults.sh
@@ -55,6 +55,7 @@ killall Dock
 defaults write com.apple.WindowManager EnableStandardClickToShowDesktop -bool false
 osascript -e 'tell application "System Events" to tell every desktop to set picture to "/System/Library/Desktop Pictures/Solid Colors/Black.png"'
 osascript -e 'tell application "System Events" to tell appearance preferences to set dark mode to false'
+defaults write com.apple.screencapture location ~/Documents/Screenshots
 
 # Rectangle
 defaults write com.knollsoft.Rectangle almostMaximizeHeight -float 1
