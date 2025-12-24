fe924bc110011ca300ce106523877346c9d791df
Anh Pham <anhpt379@gmail.com>
2025-12-24 22:09:45 +0100

    refactor(nvim): update lua config to latest recommended syntax

--- local/nvim/.config/nvim/config.lua
+++ local/nvim/.config/nvim/config.lua
@@ -32,7 +32,7 @@ require("flash").setup({
 })
 
 -- language servers
-require("mason").setup {
+require("mason").setup({
   ui = {
     icons = {
       package_installed = "✓",
@@ -40,9 +40,9 @@ require("mason").setup {
       package_uninstalled = "✗"
     }
   }
-}
+})
 
-require('mason-tool-installer').setup {
+require('mason-tool-installer').setup({
   ensure_installed = {
     'bash-language-server',
     'css-lsp',
@@ -62,11 +62,11 @@ require('mason-tool-installer').setup {
   auto_update = true,
   run_on_start = true,
   start_delay = 3000, -- 3 second delay
-}
+})
 
 require("mason-lspconfig").setup_handlers({
-  function (server_name)
-    if server_name == 'sumneko_lua' then
+  function(server_name)
+    if server_name == 'lua_ls' then
       vim.lsp.config(server_name, {
         settings = {
           Lua = {
@@ -87,19 +87,15 @@ require("mason-lspconfig").setup_handlers({
     else
       vim.lsp.config(server_name, {})
     end
-
-    vim.lsp.enable({server_name})
+    vim.lsp.enable(server_name)
   end,
 })
 
 require('lsp-status').register_progress()
-require('lspconfig')
 
 -- nvim-cmp
 vim.o.pumheight = 15  -- nvim-cmp popup height
-
 local cmp = require('cmp')
-
 local kind_icons = {
   Text = '󰙩 ',
   Method = '',
@@ -130,7 +126,7 @@ local kind_icons = {
 
 cmp.setup({
   preselect = cmp.PreselectMode.None,
-  completion = { },
+  completion = {},
   matching = {
     disallow_fuzzy_matching = false,
     disallow_partial_matching = false,
@@ -165,20 +161,17 @@ cmp.setup({
       -- vim.fn['UltiSnips#Anon'](args.body) -- For `ultisnips` users.
     end,
   },
-  mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<Tab>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({
-      i = cmp.mapping.abort(),
-      c = cmp.mapping.close(),
-    }),
+    ['<C-y>'] = cmp.config.disable,
+    ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = false }),
-  },
+  }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
     { name = 'nvim_lsp_signature_help' },
@@ -196,25 +189,12 @@ cmp.setup({
         context_after = 0,
       }
     },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
   })
 })
 
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---   mapping = cmp.mapping.preset.cmdline(),
---   sources = cmp.config.sources({
---     { name = 'cmdline' },
---     { name = 'cmdline_history' },
---   })
--- })
-
+-- lsp-status
 local lsp_status = require('lsp-status')
 lsp_status.register_progress()
-
 lsp_status.config({
   current_function = false,
   kind_labels = kind_icons,
@@ -250,6 +230,7 @@ local sources = {
   null_ls.builtins.formatting.cmake_format,
   null_ls.builtins.formatting.nginx_beautifier,
 }
+
 null_ls.setup({
   sources = sources,
   debug = true,
@@ -287,7 +268,6 @@ local languagetool = {
     }),
   }),
 }
-
 null_ls.register(languagetool)
 
 -- diagnostics
@@ -322,7 +302,9 @@ require('nvim-ts-autotag').setup()
 
 -- nvim-ts-context-commentstring
 vim.g.skip_ts_context_commentstring_module = true
-require('ts_context_commentstring').setup()
+require('ts_context_commentstring').setup({
+  enable_autocmd = false,
+})
 
 -- Treesitter
 require('nvim-treesitter.configs').setup({
@@ -410,16 +392,13 @@ require('nvim-treesitter.configs').setup({
   textobjects = {
     select = {
       enable = true,
-
       -- Automatically jump forward to textobj, similar to targets.vim
       lookahead = true,
-
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
         ["af"] = "@function.outer",
         ["if"] = "@function.inner",
         ["ac"] = "@class.outer",
-        -- you can optionally set descriptions to the mappings (used in the desc parameter of nvim_buf_set_keymap
         ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
       },
       -- You can choose the select mode (default is charwise 'v')
@@ -428,10 +407,6 @@ require('nvim-treesitter.configs').setup({
         ['@function.outer'] = 'V', -- linewise
         ['@class.outer'] = '<c-v>', -- blockwise
       },
-      -- If you set this to `true` (default is `false`) then any textobject is
-      -- extended to include preceding xor succeeding whitespace. Succeeding
-      -- whitespace has priority in order to act similarly to eg the built-in
-      -- `ap`.
       include_surrounding_whitespace = false,
     },
   },
@@ -453,7 +428,6 @@ require('hlargs').setup({
   color = '#28c9ff',
 })
 require('nvim-surround').setup()
-
 require("nvim-navic").setup({
   lsp = {
     auto_attach = true,
@@ -461,143 +435,3 @@ require("nvim-navic").setup({
   lazy_update_context = false,
 })
 
-
--- local cmp = require 'blink.cmp'
--- -- local luasnip = require 'luasnip'
-
--- cmp.setup {
---   -- TODO:
---   -- 'default' for mappings similar to built-in completion
---   -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
---   -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
---   -- see the "default configuration" section below for full documentation on how to define
---   -- your own keymap.
---   keymap = {
---     preset = 'super-tab',
---     -- -- Accept selection or next item is nothing is selected.
---     ['<C-space>'] = { 'select_and_accept', 'fallback' },
---     -- Accept if an item is selected.
---     ['<CR>'] = { 'accept', 'fallback' },
---     -- Accept if an item is selected, else snippet_forward, or fallback.
---     ['<C-k>'] = { 'select_prev', 'fallback' },
---     ['<C-j>'] = { 'select_next', 'fallback' },
-
---     ['<Up>'] = { 'select_prev', 'fallback' },
---     ['<Down>'] = { 'select_next', 'fallback' },
---   },        -- experimental auto-brackets support
---   accept = {
---     auto_brackets = { enabled = true },
---     -- expand_snippet = luasnip.lsp_expand,
---   },
-
---   -- experimental signature help support
---   trigger = {
---     -- completion = { show_in_snippet = false },
---     signature_help = { enabled = true },
---   },
-
---   fuzzy = {
---     use_typo_resistance = false,
---   },
---   sources = {
---     completion = {
---       -- Static list of providers to enable, or a function to dynamically enable/disable providers based on the context
---       enabled_providers = { 'lsp', 'path', 'snippets', 'buffer', 'ripgrep', 'mocword' },
---       -- Example dynamically picking providers based on the filetype and treesitter node:
---       -- enabled_providers = function(ctx)
---       --   local node = vim.treesitter.get_node()
---       --   if vim.bo.filetype == 'lua' then
---       --     return { 'lsp', 'path' }
---       --   elseif node and vim.tbl_contains({ 'comment', 'line_comment', 'block_comment' }), node:type())
---       --     return { 'buffer' }
---       --   else
---       --     return { 'lsp', 'path', 'snippets', 'buffer' }
---       --   end
---       -- end
---     },
-
---     -- Please see https://github.com/Saghen/blink.compat for using `nvim-cmp` sources
---     providers = {
---       lsp = {
---         name = 'LSP',
---         module = 'blink.cmp.sources.lsp',
---         --- *All* of the providers have the following options available
---         --- NOTE: All of these options may be functions to get dynamic behavior
---         --- See the type definitions for more information.
---         --- Check the enabled_providers config for an example
---         enabled = true, -- Whether or not to enable the provider
---         transform_items = nil, -- Function to transform the items before they're returned
---         should_show_items = true, -- Whether or not to show the items
---         max_items = nil, -- Maximum number of items to display in the menu
---         min_keyword_length = 0, -- Minimum number of characters in the keyword to trigger the provider
---         fallback_for = {}, -- If any of these providers return 0 items, it will fallback to this provider
---         score_offset = 0, -- Boost/penalize the score of the items
---         override = nil, -- Override the source's functions
---       },
---       path = {
---         name = 'Path',
---         module = 'blink.cmp.sources.path',
---         score_offset = 3,
---         opts = {
---           trailing_slash = false,
---           label_trailing_slash = true,
---           get_cwd = function(context) return vim.fn.expand(('#%d:p:h'):format(context.bufnr)) end,
---           show_hidden_files_by_default = false,
---         }
---       },
---       snippets = {
---         name = 'Snippets',
---         module = 'blink.cmp.sources.snippets',
---         score_offset = -3,
---         opts = {
---           friendly_snippets = true,
---           search_paths = { vim.fn.stdpath('config') .. '/snippets' },
---           global_snippets = { 'all' },
---           extended_filetypes = {},
---           ignored_filetypes = {},
---           get_filetype = function(context)
---             return vim.bo.filetype
---           end
---         }
-
---         --- Example usage for disabling the snippet provider after pressing trigger characters (i.e. ".")
---         -- enabled = function(ctx)
---         --   return ctx ~= nil and ctx.trigger.kind == vim.lsp.protocol.CompletionTriggerKind.TriggerCharacter
---         -- end,
---       },
---       buffer = {
---         name = 'Buffer',
---         module = 'blink.cmp.sources.buffer',
---         fallback_for = { 'lsp' },
---       },
---       ripgrep = {
---         module = "blink-ripgrep",
---         name = "ripgrep",
---         opts = {
---           prefix_min_len = 2,
---         }
---       },
---       mocword = {
---         name = 'mocword',
---         module = 'blink.compat.source',
---         opts = {
---           debug = true,
---         }
---       }
---     },
---   },
---   windows = {
---     autocomplete = {
---       auto_show = true,
---       selection = 'auto_insert',
---     },
---     documentation = {
---       auto_show = true,
---       auto_show_delay_ms = 300,
---       update_delay_ms = 50,
---     },
---     ghost_text = {
---       enabled = false,
---     },
---   },
--- }
